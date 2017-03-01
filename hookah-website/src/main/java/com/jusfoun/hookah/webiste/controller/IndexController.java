1b071953fe203a3ff5656b8e5acf4d66b6b12283
邵建双 <jsshao1986@hotmail.com>
2017-03-01 17:29:19 +0800

    设置数据库访问操作 2、移动mybatis generator

--- hookah-website/src/main/java/com/jusfoun/hookah/webiste/controller/IndexController.java
+++ hookah-website/src/main/java/com/jusfoun/hookah/webiste/controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.jusfoun.hookah.webiste.controller;
 
+import com.jusfoun.hookah.core.domain.Test;
+import com.jusfoun.hookah.rpc.api.TestService;
 import com.jusfoun.hookah.rpc.api.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
@@ -7,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 
@@ -23,6 +26,9 @@ public class IndexController {
     @Resource
     UserService userService;
 
+    @Resource
+    TestService testService;
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String home(Model model) {
         model.addAttribute("happy", "Hello,world");
@@ -37,6 +43,23 @@ public class IndexController {
         String s = userService.sayhello();
         System.out.println(s);
         return "index";
+    }
+
+    @RequestMapping(value = "/select", method = RequestMethod.GET)
+    @ResponseBody
+    public Object select(Model model) {
+        Test test = (Test) testService.selectById("ee");
+
+        return test;
+    }
+
+    @RequestMapping(value = "/insert", method = RequestMethod.GET)
+    @ResponseBody
+    public Object insert(Model model) {
+        Test t2 = new Test();
+        t2.setName("wwwwww");
+        testService.insert(t2);
+        return "success";
     }
 
 }
