eca94551bc7e25ec016b4eb5b6b75f6d03f23da6
黄磊 <elvis.huanglei@gmail.com>
2017-03-06 10:59:15 +0800

    交易中心exchange

---
+++ hookah-website/src/main/java/com/jusfoun/hookah/webiste/controller/ExchangeController.java
@@ -0,0 +1,19 @@
+package com.jusfoun.hookah.webiste.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author huang lei
+ * @date 2017/3/6 上午10:52
+ * @desc
+ */
+@Controller
+public class ExchangeController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index(){
+        return "exchange/index";
+    }
+}
