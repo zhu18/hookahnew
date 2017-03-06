7ef639a2981b834836eb455454e2679e7353d003
黄磊 <elvis.huanglei@gmail.com>
2017-03-06 15:27:11 +0800

    双创中心 http://localhost:9000/innovation 资讯中心 http://localhost:9000/information 博览会 http://localhost:9000/exposition

---
+++ hookah-website/src/main/java/com/jusfoun/hookah/webiste/controller/InnovationController.java
@@ -0,0 +1,19 @@
+package com.jusfoun.hookah.webiste.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author huang lei
+ * @date 2017/3/6 下午3:18
+ * @desc
+ */
+@Controller
+public class InnovationController {
+
+    @RequestMapping(value = "/innovation", method = RequestMethod.GET)
+    public String index(){
+        return "innovation/index";
+    }
+}
