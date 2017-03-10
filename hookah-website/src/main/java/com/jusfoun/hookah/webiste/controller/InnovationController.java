5a808f2c08621f4ca82366904d24fdf808126642
ky <kyy@jusfoun.com>
2017-03-10 19:22:48 +0800

    双创中心部分静态页面

--- hookah-website/src/main/java/com/jusfoun/hookah/webiste/controller/InnovationController.java
+++ hookah-website/src/main/java/com/jusfoun/hookah/webiste/controller/InnovationController.java
@@ -13,13 +13,29 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class InnovationController {
 
     @RequestMapping(value = "/innovation", method = RequestMethod.GET)
-    public String index(){
-        return "innovation/index";
-    }
+    public String index(){return "innovation/index";    }
+
+    @RequestMapping(value = "/innovation/data_crowdsourcing", method = RequestMethod.GET)
+    public String datacrowdsourcing(){return "innovation/data_crowdsourcing";    }
+
+    @RequestMapping(value = "/innovation/incubator", method = RequestMethod.GET)
+    public String incubator(){return "innovation/incubator";    }
+
+    @RequestMapping(value = "/innovation/base_development_platform", method = RequestMethod.GET)
+    public String baseplatform(){return "innovation/base_development_platform";    }
 
     @RequestMapping(value = "/innovation/equity", method = RequestMethod.GET)
-    public String equity(){
-        return "innovation/equity";
-    }
+    public String equity(){return "innovation/equity";    }
 
+    @RequestMapping(value = "/innovation/good_team", method = RequestMethod.GET)
+    public String goodteam(){return "innovation/good_team";    }
+
+    @RequestMapping(value = "/innovation/bigdata_industry", method = RequestMethod.GET)
+    public String bigdataindustry(){return "innovation/bigdata_industry";    }
+
+    @RequestMapping(value = "/innovation/activity", method = RequestMethod.GET)
+    public String activity(){return "innovation/activity";    }
+
+    @RequestMapping(value = "/innovation/guest_competition", method = RequestMethod.GET)
+    public String guestcompetition(){return "innovation/guest_competition";    }
 }
