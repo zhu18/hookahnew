557b261128f2c00c9ac1140d0976fe0b877c48da
ky <kyy@jusfoun.com>
2017-03-11 16:40:38 +0800

    双创中心静态页

--- hookah-website/src/main/java/com/jusfoun/hookah/webiste/controller/ExpositionController.java
+++ hookah-website/src/main/java/com/jusfoun/hookah/webiste/controller/ExpositionController.java
@@ -16,4 +16,18 @@ public class ExpositionController {
     public String index(){
         return "exposition/index";
     }
+
+    @RequestMapping(value = "/exposition/data_trading", method = RequestMethod.GET)
+    public String datatrading(){
+        return "exposition/data_trading";
+    }
+
+    @RequestMapping(value = "/exposition/data_crowdsourcing", method = RequestMethod.GET)
+    public String datacrowdsourcing(){return "exposition/data_crowdsourcing";}
+
+    @RequestMapping(value = "/exposition/financial_innovation", method = RequestMethod.GET)
+    public String financialinnovation(){ return "exposition/financial_innovation"; }
+
+    @RequestMapping(value = "/exposition/solve_plan", method = RequestMethod.GET)
+    public String solveplan(){ return "exposition/solve_plan"; }
 }
