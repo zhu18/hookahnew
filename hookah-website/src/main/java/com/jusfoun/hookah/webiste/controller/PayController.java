eb5063b3a40dcf7493f126cc7460914c4f962a69
吴冰冰 <codeices@163.com>
2017-03-11 11:44:47 +0800

    添加支付页面

---
+++ hookah-website/src/main/java/com/jusfoun/hookah/webiste/controller/PayController.java
@@ -0,0 +1,24 @@
+package com.jusfoun.hookah.webiste.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author bingbing wu
+ * @date 2017/3/10 下午21:21
+ * @desc
+ */
+@Controller
+@RequestMapping("/pay")
+public class PayController {
+
+    @RequestMapping(value = "/cash", method = RequestMethod.GET)
+    public String cash() {
+        return "/pay/cash";
+    }
+
+
+}
