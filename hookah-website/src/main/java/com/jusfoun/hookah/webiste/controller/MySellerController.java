7ac2e9b20eec8bed8677df59d068ab57d0942600
zoro <814737727@qq.com>
2017-03-22 18:29:34 +0800

    新增商品增删改查

--- hookah-website/src/main/java/com/jusfoun/hookah/webiste/controller/MySellerController.java
+++ hookah-website/src/main/java/com/jusfoun/hookah/webiste/controller/MySellerController.java
@@ -1,15 +1,26 @@
 package com.jusfoun.hookah.webiste.controller;
 
+import com.jusfoun.hookah.core.common.Pagination;
+import com.jusfoun.hookah.core.constants.HookahConstants;
+import com.jusfoun.hookah.core.domain.Goods;
+import com.jusfoun.hookah.core.domain.mongo.MgGoods;
 import com.jusfoun.hookah.core.domain.vo.GoodsVo;
+import com.jusfoun.hookah.core.generic.Condition;
+import com.jusfoun.hookah.core.generic.OrderBy;
 import com.jusfoun.hookah.rpc.api.CategoryService;
 import com.jusfoun.hookah.rpc.api.GoodsService;
 import com.jusfoun.hookah.rpc.api.MgCategoryAttrTypeService;
+import com.jusfoun.hookah.rpc.api.MgGoodsService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author huang lei
@@ -24,16 +35,41 @@ public class MySellerController {
     MgCategoryAttrTypeService mgCategoryAttrTypeService;
     @Resource
     GoodsService goodsService;
+    @Resource
+    MgGoodsService mgGoodsService;
 
     @RequestMapping(value = "/myseller", method = RequestMethod.GET)
     public String index(Model model){
         // 默认加载分类
-        model.addAttribute("categoryList", categoryService.getCatTree());
+        try{
+            model.addAttribute("categoryList", categoryService.getCatTree());
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
         return "/myseller/index";
     }
 
     @RequestMapping(value = "/myseller/publish", method = RequestMethod.GET)
-    public String publish(GoodsVo obj, Model model){
+    public String publish(String id, Model model){
+        if(StringUtils.isNotBlank(id)) {
+            // 查询商品信息
+            List<Condition> filters = new ArrayList<>();
+            filters.add(Condition.eq("goodsId", id));
+            filters.add(Condition.eq("domainId", "123"));
+            filters.add(Condition.in("goodsStatus", new Byte[]{1, 2}));
+            Goods goods = goodsService.selectOne(filters);
+            if (goods != null && StringUtils.isNotBlank(goods.getGoodsId())) {
+                GoodsVo goodsVo = new GoodsVo();
+                BeanUtils.copyProperties(goods, goodsVo);
+                MgGoods mgGoods = mgGoodsService.selectById(id);
+                if (mgGoods != null) {
+                    goodsVo.setFormatList(mgGoods.getFormatList());
+                    goodsVo.setImgList(mgGoods.getImgList());
+                    goodsVo.setAttrTypeList(mgGoods.getAttrTypeList());
+                    model.addAttribute("goodsInfo", goodsVo);
+                }
+            }
+        }
         return "/myseller/publish";
     }
 
@@ -43,7 +79,37 @@ public class MySellerController {
     }
 
     @RequestMapping(value = "/myseller/tradeing", method = RequestMethod.GET)
-    public String tradeing(){ return "/myseller/tradeing"; }
+    public String tradeing(String pageNumber, String pageSize, String goodsName, Byte checkStatus, Model model){
+        Pagination<Goods> page = new Pagination<>();
+        try {
+            List<Condition> filters = new ArrayList();
+            List<OrderBy> orderBys = new ArrayList();
+            orderBys.add(OrderBy.desc("lastUpdateTime"));
+            //参数校验
+            int pageNumberNew = HookahConstants.PAGE_NUM;
+            if(StringUtils.isNotBlank(pageNumber)){
+                pageNumberNew = Integer.parseInt(pageNumber);
+            }
+            int pageSizeNew = HookahConstants.PAGE_SIZE;
+            if(StringUtils.isNotBlank(pageSize)){
+                pageSizeNew = Integer.parseInt(pageSize);
+            }
+            //只查询商品状态为发布和下架的商品
+            filters.add(Condition.in("goodsStatus", new Byte[]{1,2}));
+
+            if( StringUtils.isNotBlank(goodsName)){
+                filters.add(Condition.like("goodsName", goodsName.trim()));
+            }
+            if(checkStatus != null){
+                filters.add(Condition.like("checkStatus", checkStatus));
+            }
+            page = goodsService.getListInPage(pageNumberNew, pageSizeNew, filters, orderBys);
+            model.addAttribute("pageInfo", page);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "/myseller/tradeing";
+    }
 
     @RequestMapping(value = "/myseller/rate", method = RequestMethod.GET)
     public String mysellrate(){
