80ef7c21e243be4361ebdfa45c7446be7520bb05
zoro <814737727@qq.com>
2017-03-24 15:02:15 +0800

    调整商品属性查询BUG

--- hookah-website/src/main/java/com/jusfoun/hookah/webiste/controller/ShelvesBackController.java
+++ hookah-website/src/main/java/com/jusfoun/hookah/webiste/controller/ShelvesBackController.java
@@ -1,8 +1,11 @@
 package com.jusfoun.hookah.webiste.controller;
 
 import com.jusfoun.hookah.core.utils.ReturnData;
+import com.jusfoun.hookah.rpc.api.ShelvesService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * Created by wangjl on 2017-3-23.
@@ -10,12 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("shelves/back")
 public class ShelvesBackController {
+    @Resource
+    ShelvesService shelvesService;
     /**
      * 查询用户可选的货架
      * @return
      */
     @RequestMapping("optional")
     public ReturnData getOptionShelves() {
+
         return null;
     }
 }
