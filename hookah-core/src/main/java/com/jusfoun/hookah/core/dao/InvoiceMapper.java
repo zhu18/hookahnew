83d95537f44f12c08e412c14d4d3cc5cdf9d5e9f
邵建双 <jsshao1986@hotmail.com>
2017-03-27 14:32:19 +0800

    1、修改文章列表 返回值格式 2、增加发票管理

---
+++ hookah-core/src/main/java/com/jusfoun/hookah/core/dao/InvoiceMapper.java
@@ -0,0 +1,8 @@
+package com.jusfoun.hookah.core.dao;
+
+import com.jusfoun.hookah.core.domain.Invoice;
+import com.jusfoun.hookah.core.generic.GenericDao;
+
+public interface InvoiceMapper extends GenericDao<Invoice> {
+
+}
