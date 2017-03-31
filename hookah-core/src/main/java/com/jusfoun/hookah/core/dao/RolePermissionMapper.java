f51f5b5e772723d58ff6adadde6d2fa78e0a848c
邵建双 <jsshao1986@hotmail.com>
2017-03-31 18:04:12 +0800

    删除多余Mapper

--- hookah-core/src/main/java/com/jusfoun/hookah/core/dao/RolePermissionMapper.java
+++
@@ -1,8 +0,0 @@
-package com.jusfoun.hookah.core.dao;
-
-import com.jusfoun.hookah.core.domain.RolePermission;
-import com.jusfoun.hookah.core.generic.GenericDao;
-
-public interface RolePermissionMapper extends GenericDao<RolePermission> {
-
-}
--- hookah-core/src/main/java/com/jusfoun/hookah/core/dao/SysRoleMapper.java
+++
@@ -1,7 +0,0 @@
-package com.jusfoun.hookah.core.dao;
-
-import com.jusfoun.hookah.core.domain.SysRole;
-import com.jusfoun.hookah.core.generic.GenericDao;
-
-public interface SysRoleMapper extends GenericDao<SysRole> {
-}
