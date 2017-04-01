bfc5ebbcf3b4018eaaf0bd88d8152ceca729da24
邵建双 <jsshao1986@hotmail.com>
2017-04-01 09:39:29 +0800

    修改验证码样式

--- hookah-oauth2server/src/main/java/com/jusfoun/hookah/oauth2server/config/CaptchaConfig.java
+++ hookah-oauth2server/src/main/java/com/jusfoun/hookah/oauth2server/config/CaptchaConfig.java
@@ -14,15 +14,15 @@ public class CaptchaConfig {
     public DefaultKaptcha getKaptchaBean(){
         DefaultKaptcha defaultKaptcha=new DefaultKaptcha();
         Properties properties=new Properties();
-        properties.setProperty("kaptcha.border", "no");
+        properties.setProperty("kaptcha.border", "yes"); //默认为 yes
         properties.setProperty("kaptcha.border.color", "105,179,90");
         properties.setProperty("kaptcha.textproducer.font.color", "blue");
-        properties.setProperty("kaptcha.image.width", "250");
-        properties.setProperty("kaptcha.image.height", "90");
+        properties.setProperty("kaptcha.image.width", "250");  //默认为 200
+        properties.setProperty("kaptcha.image.height", "90");  //默认为 50
         properties.setProperty("kaptcha.session.key", "code");
-        properties.setProperty("kaptcha.textproducer.char.length", "4");
+        properties.setProperty("kaptcha.textproducer.char.length", "4");  //默认为 5
         properties.setProperty("kaptcha.textproducer.font.names", "宋体,楷体,微软雅黑");
-        properties.setProperty("kaptcha.textproducer.font.size", "60");
+        properties.setProperty("kaptcha.textproducer.font.size", "60"); //默认为 40px
         Config config=new Config(properties);
         defaultKaptcha.setConfig(config);
         return defaultKaptcha;
