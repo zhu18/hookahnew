2c9d1aa3fd22c9a3414433490aa3f4407201458c
黄磊 <elvis.huanglei@gmail.com>
2017-04-05 16:53:54 +0800

    后台修改，暂存

---
+++ hookah-console-server/src/main/resources/static/apps/home/index.js
@@ -0,0 +1,40 @@
+import '../../common/common';
+import homeRouting from './home.routing';
+
+const MODULE_NAME = 'Hookah';
+let app = angular.module(MODULE_NAME, [
+  'ui.router',
+  'Common'
+]);
+/**
+ * 配置路由
+ */
+app.config(homeRouting);
+
+app.run(function ($rootScope, $state) {
+  $rootScope.config = {
+    "navScene": 'main',
+    "isSidebarFold": false,
+    "disableNavigation": false,
+    "hideSidebar": false,
+    "sidebar": "full",
+    "productNavBar": "col-1"
+  };
+  $rootScope.navConfig = {
+    "product": {
+      "show": true,
+      "folded": false
+    },
+    "service": {
+      "show": true,
+      "folded": false
+    },
+    "system": {
+      "show": true,
+      "folded": false
+    }
+  };
+  console.log("app run");
+});
+
+export default MODULE_NAME;
