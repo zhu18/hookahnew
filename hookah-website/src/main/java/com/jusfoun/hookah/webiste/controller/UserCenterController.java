8df05ff142426972f72af30ae8f1a70654dae71e
吴冰冰 <codeices@163.com>
2017-04-06 09:57:06 +0800

    修改用户中心菜单

--- hookah-website/src/main/java/com/jusfoun/hookah/webiste/controller/UserCenterController.java
+++ hookah-website/src/main/java/com/jusfoun/hookah/webiste/controller/UserCenterController.java
@@ -41,15 +41,12 @@ public class UserCenterController {
     public String publishArticle() { return "1/usercenter/publishArticle"; }
 
 
-<<<<<<< HEAD
     @RequestMapping(value = "/usercenter/userInfo", method = RequestMethod.GET)
     public String userInfo() { return "usercenter/userInfo"; }
-=======
 
 
     @RequestMapping(value = "/usercenter", method = RequestMethod.GET)
     public String index(){
         return "usercenter/myseller/goodsManage";
     }
->>>>>>> 4d6168bfb3c26b5675b9230daa230e4eb60cca66
 }
