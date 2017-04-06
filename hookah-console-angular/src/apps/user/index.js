46e8913f3fd783e09db544996147a948513bd1ae
黄磊 <elvis.huanglei@gmail.com>
2017-04-06 19:25:55 +0800

    后台管理增加用户管理模块

---
+++ hookah-console-angular/src/apps/user/index.js
@@ -0,0 +1,50 @@
+import '../../common/common';
+import userRouting from './user.routing';
+const MODULE_NAME = 'Hookah';
+let app = angular.module(MODULE_NAME, [
+  'ui.router',
+  'Common'
+]);
+app.config(userRouting);
+app.config(['$httpProvider', function ($httpProvider) {
+  $httpProvider.defaults.withCredentials = true;
+}]);
+app.run(function ($rootScope, $state) {
+  $rootScope.currentProductId = 'user';
+  $rootScope.config = {
+    "navScene": 'main',
+    "isSidebarFold": false,
+    "disableNavigation": false,
+    "hideSidebar": false,
+    "sidebar": "full",
+    "productNavBar": "col-1"
+  };
+  $rootScope.navConfig = {
+    "product": {
+      "show": true,
+      "folded": false
+    },
+    "service": {
+      "show": true,
+      "folded": false
+    },
+    "system": {
+      "show": true,
+      "folded": false
+    }
+  };
+  $rootScope.config.title = '用户管理';
+  $rootScope.config.mainNav =[
+    {
+      "title":"用户查询",
+      "url":"user.search"
+    },
+    {
+      "title":"新增用户",
+      "url":"user.add"
+    }
+  ];
+
+});
+
+export default MODULE_NAME;
