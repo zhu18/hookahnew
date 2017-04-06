3d8a2f4bf906b4b0998427e6537eea03db036ed2
lushanshan <409071865@qq.com>
2017-04-06 22:12:43 +0800

    'jiaohu'

--- hookah-website/src/main/resources/static/js/exchange/common.js
+++ hookah-website/src/main/resources/static/js/exchange/common.js
@@ -3,20 +3,53 @@
  */
 
 $(function () {
-
-    console.log(window.location.pathname);
     var url=window.location.pathname;
+    $(".exchange-index-menu div").css('display', 'none');
     if(url=='/exchange/list' || url=='/exchange/details'){
     $("#exchange_menu").hover(function () {
-    console.log(2);
     $(".exchange-index-menu").css('display','block')
     });
+
     $(".exchange-index-menu").mouseleave(function () {
-    console.log(3);
     $(".exchange-index-menu").css('display','none')
     })
+
     $(".exchange-index-menu").css('display','none');
+
     }else {
         $(".exchange-index-menu").css('display', 'block');
     }
+
+    $(".exchange-index-menu li").hover(function () {
+        var index=$(this).index();
+        var height=$(this).height()+1;
+        var _this=$(this);
+
+        $(this).parent().siblings().css({
+            "top":index*height,
+            'display':"block"
+        }).mouseleave(function () {
+            $(this).css({
+                'display':"none"
+            })
+            _this.css({
+                'backgroundColor':'#3E557B',
+                 'color':'white'
+            })
+        });
+        $(this).css({
+            'backgroundColor':'white',
+            'color':'#000000'
+        }).siblings().css({
+            'backgroundColor':'#3E557B',
+            'color':'white'
+        })
+        // $(".exchange-index-menu div").mouseleave(function () {
+        //     $(this).css({
+        //         'backgroundColor':'#3E557B',
+        //         'color':'white'
+        //     })
+        // })
+    })
+
 })
