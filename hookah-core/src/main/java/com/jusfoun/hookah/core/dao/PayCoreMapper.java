ff42f31e414bbc0d45b82f97842c00ca831221da
zhaob <zhaob@LAPTOP-DC0I82TF>
2017-04-08 15:20:29 +0800

    支付接口调整

--- hookah-core/src/main/java/com/jusfoun/hookah/core/dao/PayCoreMapper.java
+++ hookah-core/src/main/java/com/jusfoun/hookah/core/dao/PayCoreMapper.java
@@ -19,24 +19,24 @@ public interface PayCoreMapper extends GenericDao<PayCore> {
 	@Select("select * from pay_core where order_id = #{orderId}")
 	List<PayCore> getPayCoreByOrderId(@Param("orderId") Integer orderId);
 
-	@Update("update pay_core set pay_status=#{payStatus},trade_no=#{tradeNo} where order_sn=#{orderSn}")
+	/*@Update("update pay_core set pay_status=#{payStatus},trade_no=#{tradeNo} where order_sn=#{orderSn}")*/
 	void updatePayStatusAndTradeNo(PayCore pay);
 
-	@Select("select * from pay_core where order_sn = #{orderSn}")
+	/*@Select("select * from pay_core where order_sn = #{orderSn}")*/
 	List<PayCore> getPayCoreByOrderSn(String orderSn);
 	
-	@SelectProvider(type=PayCoreProvider.class, method="findByVo")
+	/*@SelectProvider(type=PayCoreProvider.class, method="findByVo")*/
 	List<PayCore> findByVo(PayCoreVo payCoreVo);
 	
-	@Select("select sum(amount) from pay_core where  ((pay_mode=1 and income_flag=1) or (pay_mode=2 and income_flag=0)) AND pay_status=2 AND user_id=#{userId} ")
-	String findTotalConsume(String userId);
+	/*@Select("select sum(amount) from pay_core where  ((pay_mode=1 and income_flag=1) or (pay_mode=2 and income_flag=0)) AND pay_status=2 AND user_id=#{userId} ")
+	*/String findTotalConsume(String userId);
 	
-	@Select("select * from pay_core where  pay_mode=2 and income_flag=1 AND pay_status=2 AND user_id=#{userId}  order by pay_date desc")
-	List<PayCore> select3(PayCore pay);
+	/*@Select("select * from pay_core where  pay_mode=2 and income_flag=1 AND pay_status=2 AND user_id=#{userId}  order by pay_date desc")
+	*/List<PayCore> select3(PayCore pay);
 
-	@Update("update pay_core set have_checked=1 where order_sn=#{orderSn}")
-	void updateCheckedStatus(PayCore pay);
+	/*@Update("update pay_core set have_checked=1 where order_sn=#{orderSn}")
+	*/void updateCheckedStatus(PayCore pay);
 
-	@SelectProvider(type=PayCoreProvider.class, method="selectRechargeList")
+	/*@SelectProvider(type=PayCoreProvider.class, method="selectRechargeList")*/
 	Page<PayCore> selectRechargeList(RechargeVo vo);
 }
