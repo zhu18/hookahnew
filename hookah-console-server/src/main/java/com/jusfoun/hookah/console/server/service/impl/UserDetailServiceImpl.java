d6c40e4550344ad5270985c19dab09760d3f4516
黄磊 <elvis.huanglei@gmail.com>
2017-04-11 19:43:14 +0800

    UserDetailService

---
+++ hookah-console-server/src/main/java/com/jusfoun/hookah/console/server/service/impl/UserDetailServiceImpl.java
@@ -0,0 +1,9 @@
+package com.jusfoun.hookah.console.server.service.impl;
+
+/**
+ * @author huang lei
+ * @date 2017/4/11 下午6:40
+ * @desc
+ */
+public class UserDetailServiceImpl {
+}
---
+++ hookah-rpc-api/src/main/java/com/jusfoun/hookah/rpc/api/UserDetailService.java
@@ -0,0 +1,9 @@
+package com.jusfoun.hookah.rpc.api;
+
+/**
+ * @author huang lei
+ * @date 2017/4/11 下午6:39
+ * @desc
+ */
+public interface UserDetailService {
+}
