466877728487674ffc549b879826da4a1c1d5507
黄磊 <elvis.huanglei@gmail.com>
2017-04-11 19:43:49 +0800

    UserDetailServcie

--- hookah-console-server/src/main/java/com/jusfoun/hookah/console/server/service/impl/UserDetailServiceImpl.java
+++ hookah-console-server/src/main/java/com/jusfoun/hookah/console/server/service/impl/UserDetailServiceImpl.java
@@ -1,9 +1,26 @@
 package com.jusfoun.hookah.console.server.service.impl;
 
+import com.jusfoun.hookah.core.dao.UserDetailMapper;
+import com.jusfoun.hookah.core.domain.UserDetail;
+import com.jusfoun.hookah.core.generic.GenericServiceImpl;
+import com.jusfoun.hookah.rpc.api.UserDetailService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
 /**
  * @author huang lei
  * @date 2017/4/11 下午6:40
  * @desc
  */
-public class UserDetailServiceImpl {
+@Service
+public class UserDetailServiceImpl extends GenericServiceImpl<UserDetail, String> implements UserDetailService {
+    @Resource
+    UserDetailMapper userDetailMapper;
+
+    @Resource
+    public void setDao(UserDetailMapper userDetailMapper) {
+        super.setDao(userDetailMapper);
+    }
+
 }
--- hookah-rpc-api/src/main/java/com/jusfoun/hookah/rpc/api/UserDetailService.java
+++ hookah-rpc-api/src/main/java/com/jusfoun/hookah/rpc/api/UserDetailService.java
@@ -1,9 +1,12 @@
 package com.jusfoun.hookah.rpc.api;
 
+import com.jusfoun.hookah.core.domain.UserDetail;
+import com.jusfoun.hookah.core.generic.GenericService;
+
 /**
  * @author huang lei
  * @date 2017/4/11 下午6:39
  * @desc
  */
-public interface UserDetailService {
+public interface UserDetailService extends GenericService<UserDetail, String> {
 }
