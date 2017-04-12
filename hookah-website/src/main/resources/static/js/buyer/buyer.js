50649628819b29c4cef70c0cc4092d826a03ccfc
孔洋洋 <kyy@jusfoun.com>
2017-04-12 16:20:01 +0800

    订单管理buyer.js

---
+++ hookah-website/src/main/resources/static/js/buyer/buyer.js
@@ -0,0 +1,11 @@
+/**
+ * Created by ki on 2017/4/12.
+ */
+
+$(function(){
+    $(".all").on("click","li",function(){
+        var index = $(this).siblings().removeClass("active").end().addClass("active").index();
+        console.log(index);
+        $(".list").hide().eq(index).show();
+    })
+})
