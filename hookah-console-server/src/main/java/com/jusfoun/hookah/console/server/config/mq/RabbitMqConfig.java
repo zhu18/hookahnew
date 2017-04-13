619f127980cdb196ddc2b7edefcf1e2bdd5a3391
邵建双 <jsshao1986@hotmail.com>
2017-04-13 15:08:21 +0800

    修改消息队列

--- hookah-console-server/src/main/java/com/jusfoun/hookah/console/server/config/mq/RabbitMqConfig.java
+++ hookah-console-server/src/main/java/com/jusfoun/hookah/console/server/config/mq/RabbitMqConfig.java
@@ -2,6 +2,7 @@ package com.jusfoun.hookah.console.server.config.mq;
 
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
@@ -18,9 +19,6 @@ import org.springframework.context.annotation.Scope;
 @ConfigurationProperties(prefix = "spring.rabbitmq")
 public class RabbitMqConfig {
 
-    public static final String FOO_EXCHANGE   = "callback.exchange.foo";
-    public static final String FOO_ROUTINGKEY = "callback.routingkey.foo";
-    public static final String FOO_QUEUE      = "callback.queue.foo";
 
     @Value("${spring.rabbitmq.addresses}")
     private String addresses;
@@ -50,59 +48,15 @@ public class RabbitMqConfig {
     }
 
     @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
+    }
+
+    @Bean
     /** 因为要设置回调类，所以应是prototype类型，如果是singleton类型，则回调类为最后一次设置 */
     @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
     public RabbitTemplate rabbitTemplate() {
         RabbitTemplate template = new RabbitTemplate(connectionFactory());
         return template;
     }
-
-    /**
-     * 针对消费者配置
-     * 1. 设置交换机类型
-     * 2. 将队列绑定到交换机
-     *
-     *
-     FanoutExchange: 将消息分发到所有的绑定队列，无routingkey的概念
-     HeadersExchange ：通过添加属性key-value匹配
-     DirectExchange:按照routingkey分发到指定队列
-     TopicExchange:多关键字匹配
-     */
-//    @Bean
-//    public DirectExchange defaultExchange() {
-//        return new DirectExchange(FOO_EXCHANGE);
-//    }
-//
-//    @Bean
-//    public Queue queue() {
-//        return new Queue("spring-boot-queue", true); //队列持久
-//
-//    }
-//
-//    @Bean
-//    public Binding binding() {
-//        return BindingBuilder.bind(queue()).to(defaultExchange()).with(FOO_ROUTINGKEY);
-//    }
-//
-//    @Bean
-//    public SimpleMessageListenerContainer messageContainer() {
-//        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer(connectionFactory());
-//        container.setQueues(queue());
-//        container.setExposeListenerChannel(true);
-//        container.setMaxConcurrentConsumers(1);
-//        container.setConcurrentConsumers(1);
-//        container.setAcknowledgeMode(AcknowledgeMode.MANUAL); //设置确认模式手工确认
-//        container.setMessageListener(new ChannelAwareMessageListener() {
-//
-//            @Override
-//            public void onMessage(Message message, Channel channel) throws Exception {
-//                byte[] body = message.getBody();
-//                System.out.println("receive msg : " + new String(body));
-//                channel.basicAck(message.getMessageProperties().getDeliveryTag(), false); //确认消息成功消费
-//            }
-//        });
-//        return container;
-//    }
-
-
 }
