71a3625e605a1f2fa84c360635a33871cb030859
黄磊 <elvis.huanglei@gmail.com>
2017-04-13 15:20:45 +0800

    字典服务加入返回tree方法

--- hookah-console-server/src/main/java/com/jusfoun/hookah/console/server/api/sys/DictApi.java
+++ hookah-console-server/src/main/java/com/jusfoun/hookah/console/server/api/sys/DictApi.java
@@ -1,9 +1,31 @@
 package com.jusfoun.hookah.console.server.api.sys;
 
+import com.jusfoun.hookah.core.domain.Dict;
+import com.jusfoun.hookah.core.utils.ReturnData;
+import com.jusfoun.hookah.rpc.api.DictService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
 /**
  * @author huang lei
  * @date 2017/4/13 下午2:42
  * @desc
  */
+@RestController
+@RequestMapping(value = "/api/sys/dict")
 public class DictApi {
+
+    @Resource
+    private DictService dictService;
+
+    @RequestMapping(value = "/tree", method = RequestMethod.GET)
+    public ReturnData getListInPage() {
+
+        List<Dict> dictTreeList = dictService.selectTree();
+        return ReturnData.success(dictTreeList);
+    }
 }
