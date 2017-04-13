fc4fa665f00395e9c051a39e2208599684437cfd
黄磊 <elvis.huanglei@gmail.com>
2017-04-13 15:20:13 +0800

    后台系统设置

---
+++ hookah-console-angular/src/apps/sys/index.js
@@ -0,0 +1,60 @@
+import "../../common/common";
+import sysRouting from "./sys.routing";
+const MODULE_NAME = 'Hookah';
+let app = angular.module(MODULE_NAME, [
+  'Common',
+  'ui.router',
+  'ui.bootstrap'
+]);
+app.config(sysRouting);
+
+app.run(function ($rootScope, $state) {
+  $rootScope.currentProductId = 'sys';
+  $rootScope.config = {
+    "navScene": 'main',
+    "isSidebarFold": false,
+    "disableNavigation": false,
+    "hideSidebar": false,
+    "sidebar": "full",
+    "productNavBar": "col-1"
+  };
+  $rootScope.navConfig = {
+    "product": {
+      "show": true,
+      "folded": false
+    },
+    "service": {
+      "show": true,
+      "folded": false
+    },
+    "system": {
+      "show": true,
+      "folded": false
+    }
+  };
+  $rootScope.config.title = '系统设置';
+  $rootScope.config.mainNav = [
+    {
+      "title": "商品查询",
+      "url": "items.search"
+    },
+    {
+      "title": "字典管理",
+      "showChild": true,
+      "childs": [
+        {
+          "title": "字典查询",
+          "url": "sys.dict.search"
+        }
+        , {
+          "title": "新增",
+          "url": "sys.dict.add"
+        }
+      ]
+    }
+
+  ];
+
+});
+
+export default MODULE_NAME;
