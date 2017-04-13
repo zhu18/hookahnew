aac87495abd9b68db612c10ca720f04a8b2cdc21
黄磊 <elvis.huanglei@gmail.com>
2017-04-13 14:03:17 +0800

    新增广告实体类，服务类

---
+++ hookah-console-server/src/main/java/com/jusfoun/hookah/console/server/api/advert/AdvertApi.java
@@ -0,0 +1,9 @@
+package com.jusfoun.hookah.console.server.api.advert;
+
+/**
+ * @author huang lei
+ * @date 2017/4/13 下午1:50
+ * @desc
+ */
+public class AdvertApi {
+}
---
+++ hookah-core/src/main/java/com/jusfoun/hookah/core/dao/AdvertMapper.java
@@ -0,0 +1,9 @@
+package com.jusfoun.hookah.core.dao;
+
+/**
+ * @author huang lei
+ * @date 2017/4/13 下午1:45
+ * @desc
+ */
+public interface AdvertMapper {
+}
