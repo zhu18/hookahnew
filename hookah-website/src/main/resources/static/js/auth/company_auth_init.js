cc23f4ec9013c205ddb25fdd75c0409f4e189b01
孔洋洋 <kyy@jusfoun.com>
2017-04-15 13:51:15 +0800

    user_auth_init.js & company_auth_init.js

--- hookah-website/src/main/resources/static/js/auth/company_auth_init.js
+++ hookah-website/src/main/resources/static/js/auth/company_auth_init.js
@@ -3,19 +3,27 @@
  */
 //认证协议页面：点击下一步，跳转到company_auth_init_step2页面
 $(function(){
-    if(!$("#check").is(":checked")){
-        $(".agreement-btn a").attr('href','javascript:;')
-        $('.gray-btn2').css({
-            "background-color":"gray",
-            "cursor": "not-allowed"
-        })
+    judge();
+    function judge(){
+        if(!$("#check").is(":checked")){
+            $(".agreement-btn a").attr('href','javascript:;')
+            $('.gray-btn2').css({
+                "background-color":"gray",
+                "cursor": "not-allowed"
+            })
+        }
     }
     $("#check").click(function(){
-        $(".gray-btn2").css({
-            "background-color":"blue",
-            "cursor": "pointer"
-        });
-        $("#next").attr("href","./user_auth_init_step2");
+        if($("#check").is(":checked")){
+            $(".gray-btn2").css({
+                "background-color":"blue",
+                "cursor": "pointer"
+            });
+            $("#next").attr("href","./user_auth_init_step2");
+        }else{
+            judge();
+        }
+
     })
 
 })
