e1ed51e0fdabf8f22201f4d6010085e798d2bbe7
陈涛平 <ctp971412157@qq.com>
2017-04-17 18:44:10 +0800

    ctp

--- hookah-core/src/main/java/com/jusfoun/hookah/core/dao/ShelvesTypeMapper.java
+++
@@ -1,7 +0,0 @@
-package com.jusfoun.hookah.core.dao;
-
-import com.jusfoun.hookah.core.domain.ShelvesType;
-import com.jusfoun.hookah.core.generic.GenericDao;
-
-public interface ShelvesTypeMapper extends GenericDao<ShelvesType> {
-}
