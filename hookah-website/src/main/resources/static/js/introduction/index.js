fd0ec1657de63c67bb1224b91aa752db9d53ddb4
lushanshan <409071865@qq.com>
2017-04-18 18:08:19 +0800

    兼容jq'

--- hookah-website/src/main/resources/static/js/introduction/index.js
+++ hookah-website/src/main/resources/static/js/introduction/index.js
@@ -7,7 +7,6 @@ $(function(){
     console.log("url:"+url);
     $('.topList ul li').each(function () {
         var href = $(this).children('a').attr('href');
-        console.log("href:"+href);
         if (url.indexOf(href) >= 0 ) {
             $(this).addClass('active').siblings().removeClass("active");
             if(url.indexOf("introduction")){
