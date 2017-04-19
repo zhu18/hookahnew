e65f2adc1b7152647c524d2acf0620877678179d
邵建双 <jsshao1986@hotmail.com>
2017-04-19 15:22:50 +0800

    恢复代码

--- hookah-console-server/src/main/java/com/jusfoun/hookah/console/server/service/impl/MqSenderServiceImpl.java
+++ hookah-console-server/src/main/java/com/jusfoun/hookah/console/server/service/impl/MqSenderServiceImpl.java
@@ -2,16 +2,9 @@ package com.jusfoun.hookah.console.server.service.impl;
 
 import com.jusfoun.hookah.console.server.config.mq.RabbitMqSender;
 import com.jusfoun.hookah.core.constants.RabbitmqQueue;
-import com.jusfoun.hookah.core.domain.SysMessage;
 import com.jusfoun.hookah.rpc.api.MqSenderService;
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author jsshao
@@ -24,44 +17,12 @@ public class MqSenderServiceImpl implements MqSenderService {
     RabbitMqSender sender;
 
     @Override
-    public void sendTopic(String msg) {
-        SysMessage message = new SysMessage();
-        try {
-            Subject subject = SecurityUtils.getSubject();
-
-            Map userMap = null;
-            if (subject != null && subject.isAuthenticated()) {
-                userMap = (HashMap) SecurityUtils.getSubject().getSession().getAttribute("user");
-            }
-            message.setSenderId((String) userMap.get("userId"));
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        message.setCreateTime(new Date());
-        message.setMsgText(msg);
-        message.setType((short)1);
-
-        sender.sendRabbitmqTopic(RabbitmqQueue.CONTRACE_MESSAGE,message);
+    public void sendTopic(String routeKey, Object obj) {
+        sender.sendRabbitmqTopic(RabbitmqQueue.CONTRACE_MESSAGE, obj);
     }
 
     @Override
-    public void sendDirect(String msg) {
-        SysMessage message = new SysMessage();
-        try {
-            Subject subject = SecurityUtils.getSubject();
-
-            Map userMap = null;
-            if (subject != null && subject.isAuthenticated()) {
-                userMap = (HashMap) SecurityUtils.getSubject().getSession().getAttribute("user");
-            }
-            message.setSenderId((String) userMap.get("userId"));
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        message.setCreateTime(new Date());
-        message.setMsgText(msg);
-        message.setType((short)1);
-
-        sender.sendRabbitmqDirect(RabbitmqQueue.CONTRACE_MESSAGE,message);
+    public void sendDirect(String routeKey, Object obj) {
+        sender.sendRabbitmqDirect(RabbitmqQueue.CONTRACE_MESSAGE, obj);
     }
 }
