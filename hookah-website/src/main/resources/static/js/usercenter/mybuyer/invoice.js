3f858df531cc5d42fa7ef4b931ccb29f5aab6ec6
wangcaiqin <wangcaiqin@jusfoun.com>
2017-04-20 13:56:07 +0800

    fix invoce   and invoiceList

---
+++ hookah-website/src/main/resources/static/js/usercenter/mybuyer/invoice.js
@@ -0,0 +1,17 @@
+/**
+ * Created by wcq on 2017/4/20.
+ */
+// 全选
+$("body").on("click", ".checkbox-title", function () {
+    $("input[name='checkbox']").prop('checked', $(this).is(':checked'));
+    $(".checkbox-title").prop("checked", $(this).is(':checked'));
+});
+//单选
+$("body").on("click", ".checkbox", function () {
+    if ($(".checkbox:checked").length == $('.checkbox').length) {
+        $(".checkbox-title").prop("checked", true);
+    }
+    else {
+        $(".checkbox-title").prop("checked", false);
+    }
+});1111
