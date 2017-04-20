5ebd36e30a6a4b22d6e7cd9854d10cff76ff4409
陈涛平 <ctp971412157@qq.com>
2017-04-20 10:41:37 +0800

    添加后台分类管理

---
+++ hookah-console-server/src/main/java/com/jusfoun/hookah/console/server/api/goodsAttr/GoodsAttrTypeApi.java
@@ -0,0 +1,73 @@
+package com.jusfoun.hookah.console.server.api.goodsAttr;
+
+import com.jusfoun.hookah.core.domain.Category;
+import com.jusfoun.hookah.core.domain.GoodsAttrType;
+import com.jusfoun.hookah.core.exception.HookahException;
+import com.jusfoun.hookah.core.utils.ExceptionConst;
+import com.jusfoun.hookah.core.utils.ReturnData;
+import com.jusfoun.hookah.rpc.api.GoodsAttrTypeService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * Created by ctp
+ */
+
+@RestController
+@RequestMapping("/api/attrType")
+public class GoodsAttrTypeController {
+
+    @Resource
+    GoodsAttrTypeService goodsAttrTypeService;
+
+    /**
+     * 新增商品属性分类
+     * @param obj
+     * @return
+     */
+    @RequestMapping("add")
+    public ReturnData addAttrType(GoodsAttrType obj) {
+        ReturnData<List<Category>> returnData = new ReturnData<>();
+        returnData.setCode(ExceptionConst.Success);
+        try {
+            obj = goodsAttrTypeService.insert(obj);
+            if(obj == null) {
+                throw new HookahException("操作失败");
+            }
+        }catch (Exception e) {
+            returnData.setCode(ExceptionConst.Error);
+            returnData.setMessage(e.toString());
+            e.printStackTrace();
+        }
+        return returnData;
+    }
+
+    /**
+     * 删除商品属性分类
+     * @param id
+     * @return
+     */
+    @RequestMapping("del")
+    public ReturnData delAttrType(String id, String shopId) {
+        ReturnData<List<GoodsAttrType>> returnData = new ReturnData<>();
+        returnData.setCode(ExceptionConst.Success);
+        try {
+            //TODO 判断属性分类下是否有商品
+            GoodsAttrType attrType = new GoodsAttrType();
+            attrType.setTypeId(id);
+            attrType.setDomainId(shopId);
+            int i = goodsAttrTypeService.delete(attrType);
+            if(i < 0) {
+                throw new HookahException("操作失败");
+            }
+        }catch (Exception e) {
+            returnData.setCode(ExceptionConst.Error);
+            returnData.setMessage(e.toString());
+            e.printStackTrace();
+        }
+        return returnData;
+    }
+}
