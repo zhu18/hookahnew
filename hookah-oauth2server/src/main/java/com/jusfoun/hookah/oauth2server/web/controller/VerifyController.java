0a7976761fa5c4fdcd2ef38ac14080e558c948ee
吴冰冰 <codeices@163.com>
2017-04-21 15:44:51 +0800

    Verify controller

--- hookah-oauth2server/src/main/java/com/jusfoun/hookah/oauth2server/web/controller/VerifyController.java
+++ hookah-oauth2server/src/main/java/com/jusfoun/hookah/oauth2server/web/controller/VerifyController.java
@@ -1,9 +1,16 @@
 package com.jusfoun.hookah.oauth2server.web.controller;
 
+import com.jusfoun.hookah.core.domain.User;
+import com.jusfoun.hookah.rpc.api.UserService;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.session.Session;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
 
 
 /**
@@ -13,8 +20,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class VerifyController {
+
+    @Resource
+    UserService userService;
+
     @RequestMapping(value = "/verify", method = RequestMethod.GET)
     public String search(Model model) {
+        Session session = SecurityUtils.getSubject().getSession();
+        HashMap<String,String> userMap = (HashMap<String,String>)session.getAttribute("user");
+        User user = userService.selectById(userMap.get("userId"));
+        model.addAttribute("userObj",user);
         model.addAttribute("title","验证身份");
         return "modify/verify";
     }
