26214b5333ad9fbe9e4f136828265ea0fe3db306
zoro <814737727@qq.com>
2017-04-24 16:04:47 +0800

    代码调整

--- hookah-core/src/main/java/com/jusfoun/hookah/core/dao/GoodsMapper.java
+++ hookah-core/src/main/java/com/jusfoun/hookah/core/dao/GoodsMapper.java
@@ -11,6 +11,7 @@ public interface GoodsMapper extends GenericDao<Goods> {
     List<EsGoods> getNeedEsGoods();
 
     // 更新商品下架,商品下架清空商品上架时间
-    @Update("update goods set is_onsale = 0, onsale_end_date = SYSDATE(), onsale_start_date = null where goods_id = #{id}")
+    @Update("update goods set is_onsale = 0, onsale_end_date = SYSDATE(), onsale_start_date = null, " +
+            "check_status = 0 where goods_id = #{id}")
     int updateOffSale(String id);
 }
