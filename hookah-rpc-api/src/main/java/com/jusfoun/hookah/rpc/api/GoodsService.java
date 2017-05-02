e8a0f6deb2542c74e93c3ad11f3c588a0108842d
zoro <814737727@qq.com>
2017-05-02 09:01:17 +0800

    修改商品审核流程，增加查询列表

--- hookah-rpc-api/src/main/java/com/jusfoun/hookah/rpc/api/GoodsService.java
+++ hookah-rpc-api/src/main/java/com/jusfoun/hookah/rpc/api/GoodsService.java
@@ -1,10 +1,13 @@
 package com.jusfoun.hookah.rpc.api;
 
+import com.jusfoun.hookah.core.common.Pagination;
 import com.jusfoun.hookah.core.domain.Goods;
 import com.jusfoun.hookah.core.domain.mongo.MgGoods;
 import com.jusfoun.hookah.core.domain.vo.GoodsVo;
 import com.jusfoun.hookah.core.exception.HookahException;
 import com.jusfoun.hookah.core.generic.GenericService;
+
+import java.util.List;
 
 /**
  * @author huang lei
@@ -20,4 +23,12 @@ public interface GoodsService extends GenericService<Goods,String> {
     int updateGoodsStatus(String goodsId, String status);
 
     int onsale(String goodsId, String dateTime);
+
+    Pagination saleList(String pageNum, String pageSize, String goodsName, String userId);
+
+    List waitList(String goodsName, String userId, Integer checkStatus, Integer isBook);
+
+    Pagination offsaleList(String pageNum, String pageSize, String goodsName, String userId);
+
+    Pagination illegalList(String pageNum, String pageSize, String goodsName, String userId);
 }
