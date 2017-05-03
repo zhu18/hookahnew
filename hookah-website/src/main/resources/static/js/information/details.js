10f1b044a8a4f34f9af6d512d23b03c90bf05e1a
kongyang <kyy@jusfoun.com>
2017-05-03 14:57:36 +0800

    information/details.js

--- hookah-website/src/main/resources/static/js/information/details.js
+++ hookah-website/src/main/resources/static/js/information/details.js
@@ -1,3 +1,4 @@
+// details详情页
 $.getUrlParam = function (key) {
     var reg = new RegExp("(^|&)" + key + "=([^&]*)(&|$)");
     var result = window.location.search.substr(1).match(reg);
@@ -19,11 +20,20 @@ function renderDetails(){
                 $('#newsTime').html(format(data.data.sytTime));
                 $('#content').html(data.data.content);
                 $('#userImg').attr('src',data.data.headImg);
+                if(data.data.newsGroup=="information"){
+                    $(".detail .top .text1").text("资讯中心");
+                }
+                if(data.data.newsSonGroup =="1"){
+                    $(".detail .top .text2").text("中心动态");
+                }else if(data.data.newsSonGroup =="2"){
+                    $(".detail .top .text2").text("行业资讯");
+                }else {
+                    $(".detail .top .text2").text("中心公告");
+                }
+
             }else{
                 console.log(data.message);
             }
         }
     });
 }
-
-
