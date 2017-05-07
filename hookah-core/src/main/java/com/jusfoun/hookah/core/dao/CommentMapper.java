bba0b37b9aa0154dba8a9ca9a2a681a16196af9e
陈涛平 <ctp971412157@qq.com>
2017-05-07 19:54:08 +0800

    新增获取时间段内有评论更新的商品的评价分数

--- hookah-core/src/main/java/com/jusfoun/hookah/core/dao/CommentMapper.java
+++ hookah-core/src/main/java/com/jusfoun/hookah/core/dao/CommentMapper.java
@@ -4,6 +4,11 @@ import com.jusfoun.hookah.core.domain.Comment;
 import com.jusfoun.hookah.core.generic.GenericDao;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.Date;
+import java.util.List;
+
 public interface CommentMapper extends GenericDao<Comment> {
     Double selectGoodsAvgByGoodsId(@Param("goodsId") String goodsId);
+
+    List<String> selectGoodsIdsByData(@Param("startTime") Date startTime ,@Param("endTime") Date endTime);
 }
