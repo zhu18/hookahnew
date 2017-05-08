948b5426a409420b34c961488696a262021005fd
邓许 <mail.deng@qq.com>
2017-05-08 16:28:37 +0800

    商品分类

--- hookah-core/src/main/java/com/jusfoun/hookah/core/domain/mongo/MgGoods.java
+++ hookah-core/src/main/java/com/jusfoun/hookah/core/domain/mongo/MgGoods.java
@@ -27,7 +27,12 @@ public class MgGoods extends GenericModel {
     private List<FormatBean> formatList;
     private List<ImgBean> imgList;
     private ApiInfoBean apiInfo;
+    private DataModelBean dataModel;
+    private ApplicationSceneBean applicationScene;
+    private AloneSoftwareBean aloneSoftware;
+    private SaaSBean saaS;
     private Long clickRate;
+    private String otherDesc;
 
     public List<MgCategoryAttrType.AttrTypeBean> getAttrTypeList() {
         return attrTypeList;
@@ -71,6 +76,46 @@ public class MgGoods extends GenericModel {
 
     public ApiInfoBean getApiInfo() {
         return apiInfo;
+    }
+
+    public DataModelBean getDataModel() {
+        return dataModel;
+    }
+
+    public void setDataModel(DataModelBean dataModel) {
+        this.dataModel = dataModel;
+    }
+
+    public ApplicationSceneBean getApplicationScene() {
+        return applicationScene;
+    }
+
+    public void setApplicationScene(ApplicationSceneBean applicationScene) {
+        this.applicationScene = applicationScene;
+    }
+
+    public AloneSoftwareBean getAloneSoftware() {
+        return aloneSoftware;
+    }
+
+    public void setAloneSoftware(AloneSoftwareBean aloneSoftware) {
+        this.aloneSoftware = aloneSoftware;
+    }
+
+    public SaaSBean getSaaS() {
+        return saaS;
+    }
+
+    public void setSaaS(SaaSBean saaS) {
+        this.saaS = saaS;
+    }
+
+    public String getOtherDesc() {
+        return otherDesc;
+    }
+
+    public void setOtherDesc(String otherDesc) {
+        this.otherDesc = otherDesc;
     }
 
     public void setApiInfo(ApiInfoBean apiInfo) {
@@ -291,4 +336,194 @@ public class MgGoods extends GenericModel {
         }
     }
 
+    public static class DataModelBean implements Serializable {
+
+        // 数据模型
+        private String complexity;  // 复杂度
+        private String maturity;    // 成熟度
+        private String aexp;        // 应用经验
+        private String modelFile;   // 模型文件
+        private String configFile;  // 配置文件
+        private String configParams;    // 配置参数
+
+        public String getComplexity() {
+            return complexity;
+        }
+
+        public void setComplexity(String complexity) {
+            this.complexity = complexity;
+        }
+
+        public String getMaturity() {
+            return maturity;
+        }
+
+        public void setMaturity(String maturity) {
+            this.maturity = maturity;
+        }
+
+        public String getAexp() {
+            return aexp;
+        }
+
+        public void setAexp(String aexp) {
+            this.aexp = aexp;
+        }
+
+        public String getModelFile() {
+            return modelFile;
+        }
+
+        public void setModelFile(String modelFile) {
+            this.modelFile = modelFile;
+        }
+
+        public String getConfigFile() {
+            return configFile;
+        }
+
+        public void setConfigFile(String configFile) {
+            this.configFile = configFile;
+        }
+
+        public String getConfigParams() {
+            return configParams;
+        }
+
+        public void setConfigParams(String configParams) {
+            this.configParams = configParams;
+        }
+    }
+
+    public static class ApplicationSceneBean implements Serializable {
+
+        // 应用场景
+        private String complexity;  // 复杂度
+        private String versionDesc;  // 版本说明
+        private String serviceLevel;  // 服务等级
+        private String aexp;        // 应用经验
+        private String aintroduce;  // 应用介绍
+        private String cloudHardwareResource;  // 云硬件资源
+
+        public String getComplexity() {
+            return complexity;
+        }
+
+        public void setComplexity(String complexity) {
+            this.complexity = complexity;
+        }
+
+        public String getVersionDesc() {
+            return versionDesc;
+        }
+
+        public void setVersionDesc(String versionDesc) {
+            this.versionDesc = versionDesc;
+        }
+
+        public String getServiceLevel() {
+            return serviceLevel;
+        }
+
+        public void setServiceLevel(String serviceLevel) {
+            this.serviceLevel = serviceLevel;
+        }
+
+        public String getAexp() {
+            return aexp;
+        }
+
+        public void setAexp(String aexp) {
+            this.aexp = aexp;
+        }
+
+        public String getAintroduce() {
+            return aintroduce;
+        }
+
+        public void setAintroduce(String aintroduce) {
+            this.aintroduce = aintroduce;
+        }
+
+        public String getCloudHardwareResource() {
+            return cloudHardwareResource;
+        }
+
+        public void setCloudHardwareResource(String cloudHardwareResource) {
+            this.cloudHardwareResource = cloudHardwareResource;
+        }
+    }
+
+    public static class SaaSBean implements Serializable {
+
+        // SaaS
+        private String industryField;       // 行业领域
+        private String versionDesc;       // 版本说明
+        private String toolsIntroduce;       // 工具介绍
+
+        public String getIndustryField() {
+            return industryField;
+        }
+
+        public void setIndustryField(String industryField) {
+            this.industryField = industryField;
+        }
+
+        public String getVersionDesc() {
+            return versionDesc;
+        }
+
+        public void setVersionDesc(String versionDesc) {
+            this.versionDesc = versionDesc;
+        }
+
+        public String getToolsIntroduce() {
+            return toolsIntroduce;
+        }
+
+        public void setToolsIntroduce(String toolsIntroduce) {
+            this.toolsIntroduce = toolsIntroduce;
+        }
+    }
+
+    public static class AloneSoftwareBean implements Serializable {
+
+        // 独立软件
+        private String AloneIndustryField;       // 行业领域
+        private String AloneVersionDesc;       // 版本说明
+        private String AloneToolsIntroduce;       // 工具介绍
+        private String AloneCloudHardwareResource;  // 云硬件资源
+
+        public String getAloneIndustryField() {
+            return AloneIndustryField;
+        }
+
+        public void setAloneIndustryField(String aloneIndustryField) {
+            AloneIndustryField = aloneIndustryField;
+        }
+
+        public String getAloneVersionDesc() {
+            return AloneVersionDesc;
+        }
+
+        public void setAloneVersionDesc(String aloneVersionDesc) {
+            AloneVersionDesc = aloneVersionDesc;
+        }
+
+        public String getAloneToolsIntroduce() {
+            return AloneToolsIntroduce;
+        }
+
+        public void setAloneToolsIntroduce(String aloneToolsIntroduce) {
+            AloneToolsIntroduce = aloneToolsIntroduce;
+        }
+
+        public String getAloneCloudHardwareResource() {
+            return AloneCloudHardwareResource;
+        }
+
+        public void setAloneCloudHardwareResource(String aloneCloudHardwareResource) {
+            AloneCloudHardwareResource = aloneCloudHardwareResource;
+        }
+    }
 }
