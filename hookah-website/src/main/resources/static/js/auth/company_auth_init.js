7631131ba2679ce4fc9045f4909a84cf6215194e
wangcaiqin <wangcaiqin@jusfoun.com>
2017-05-08 14:47:30 +0800

    renzheng

--- hookah-website/src/main/resources/static/js/auth/company_auth_init.js
+++ hookah-website/src/main/resources/static/js/auth/company_auth_init.js
@@ -34,57 +34,6 @@ $(function(){
     $("#verifyBtn").on("click",companyAuth());
 
 });
-//检查政府全称是否填写
-function governmentName(){
-    var governmentName = $("#governmentName").val();
-    if(!governmentName){
-        alert("请输入政府全称");
-    }
-}
-//检查信用代码是否为空
-function creditCode(){
-    var creditCode = $("#creditCode").val();
-    if(!creditCode){
-        alert("请输入信用代码");
-    }
-}
-//验证营业执照到期时间是否为空
-function businessLicence(){
-    var businessLicence = $("#businessLicence").val();
-    if(!businessLicence){
-        alert("请输入营业执照到期时间");
-    }
-}
-//验证税务登记证到期时间是否为空
-function taxRegCertificate(){
-    var taxRegCertificate = $("#taxRegCertificate").val();
-    if(!taxRegCertificate){
-        alert("请输入税务登记证到期时间");
-    }
-}
-//验证企业法人是否为空
-function companyLegal(){
-    var companyLegal = $("#companyLegal").val();
-    if(!companyLegal){
-        alert("请输入公司的法人代表");
-    }
-}
-//验证主营业务是否为空
-function mainBusiness(){
-    var mainBusiness = $("#mainBusiness").val();
-    if(!mainBusiness){
-        alert("请输入您企业的主营业务");
-    }
-}
-//验证详细地址是否为空
-function address(){
-    var address = $("#address").val();
-    if(!address){
-        alert("请输入企业的详细地址");
-    }
-}
-function companyAuth(){
 
-}
 
 
