31367bae219b71f68c6f5cee6146d9eb9938294e
lushanshan <409071865@qq.com>
2017-06-01 17:44:30 +0800

    zc

--- hookah-website/src/main/resources/static/js/pay/cash.js
+++ hookah-website/src/main/resources/static/js/pay/cash.js
@@ -132,6 +132,7 @@ function testPayPassword(pwd){
 				return true;
 			}else if(data.code == 0){
 				$('.ui-form-error').show().children('p').html('支付密码不正确');
+                $('#paymentPassword').val()
 				return false;
 			}else{
 				$.alert(data.message);
