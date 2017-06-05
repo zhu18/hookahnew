8399f5063d72c5aa322f9ad8a237a0114614d8ce
吴冰冰 <codeices@163.com>
2017-06-05 16:34:59 +0800

    Fix bug

--- hookah-website/src/main/resources/static/js/goods/details.js
+++ hookah-website/src/main/resources/static/js/goods/details.js
@@ -197,12 +197,13 @@ function check() {
 	});
 	// return false;
 }
-function getDataPackage(sourceId){
+function getDataPackage(goodsId,sourceId){
     $.ajax({
         url: host.website+'/help/exportWords',
         type:'get',
 		data:{
-			goodsId : sourceId,
+			goodsId : goodsId,
+			sourceId : sourceId,
 			orderNo : $.getUrlParam('orderSn')
         },
         success:function(data){
