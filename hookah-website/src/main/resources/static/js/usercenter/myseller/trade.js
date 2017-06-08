d149ade1159e6a3ab5f147b555e401ce00a7c186
吴冰冰 <codeices@163.com>
2017-06-08 15:39:06 +0800

    添加出售中商品列表

--- hookah-website/src/main/resources/static/js/usercenter/myseller/trade.js
+++ hookah-website/src/main/resources/static/js/usercenter/myseller/trade.js
@@ -3,20 +3,20 @@ function loadPageData(data){
         var list = data.data.list;
         var html = '';
         for(var i=0; i<list.length; i++){
-        	var item = list.mgOrderGoodsList;
-			for(var j=0; j < item.mgOrderGoodsList.length; j++){
+        	var item = list[i].mgOrderGoodsList;
+			for(var j=0; j < item.length; j++){
 				html += '<tr>';
 				html += '<td class="text-center">';
 				html += '<a href="/exchange/details?id='+item[j].goodsId+'">';
 				html += '<img src="'+item[j].goodsImg+'" alt="">';
-				html += '<p>'+item[j].goodsImg+'</p>';
+				html += '<p>'+item[j].goodsName+'</p>';
 				html += '</a>';
 				html += '</td>';
 				html += '<td>'+list[i].orderSn+'</td>';
 				html += '<td>'+list[i].shippingTime+'</td>';
-				html += '<td class="text-left">'+item[j].goodsPrice / 100+'</td>';
+				html += '<td class="text-left">'+(item[j].goodsPrice / 100).toFixed(2)+'</td>';
 				html += '<td class="text-center">'+item[j].goodsNumber+'</td>';
-				html += '<td class="text-left">'+(item[j].goodsPrice * item[j].goodsNumber) / 100+'</td>';
+				html += '<td class="text-left">'+((item[j].goodsPrice * item[j].goodsNumber) / 100).toFixed(2)+'</td>';
 				html += '</tr>';
 			}
         }
