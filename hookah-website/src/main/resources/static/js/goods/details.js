f567b9f86f3dcc79fd93b9c3235917f7dfe5f344
kongyang <2268560760@qq.com>
2017-06-15 13:44:09 +0800

    bug

--- hookah-website/src/main/resources/static/js/goods/details.js
+++ hookah-website/src/main/resources/static/js/goods/details.js
@@ -2,88 +2,93 @@
  * Created by lss on 2017/4/7 0007.
  */
 // 列表切换功能
-var priceT = (JshopPrice / 100).toFixed(2)
-$('#J_goodsPrice').html(priceT);
-function switchHover() {
-    $(".goods-table .table-content .table-item").each(function () {
-        if ($(this).hasClass('active')) {
-            $(this).css({
-                'display': 'block'
-            }).siblings().css({
-                'display': 'none'
-            })
-        }
-    })
-    $(".goods-table .table-title li").on('click', function () {
-        $(this).addClass('active').siblings().removeClass('active');
-        var index = $(this).index();
+$(function(){
+    function switchHover() {
         $(".goods-table .table-content .table-item").each(function () {
-            if (index == $(this).index()) {
+            if ($(this).hasClass('active')) {
                 $(this).css({
                     'display': 'block'
                 }).siblings().css({
                     'display': 'none'
                 })
             }
-        });
-        if ($(document).scrollTop() >= 570) {
-            $("body").animate({scrollTop: 570}, 300);
-        }
-    })
-}
+        })
+        $(".goods-table .table-title li").on('click', function () {
+            $(this).addClass('active').siblings().removeClass('active');
+            var index = $(this).index();
+            $(".goods-table .table-content .table-item").each(function () {
+                if (index == $(this).index()) {
+                    $(this).css({
+                        'display': 'block'
+                    }).siblings().css({
+                        'display': 'none'
+                    })
+                }
+            });
+            if ($(document).scrollTop() >= 570) {
+                $("body").animate({scrollTop: 570}, 300);
+            }
+        })
+    }
 // 购买数量功能
-function purchaseQuantity() {
-    var conut = '';
-    // 对输入框中的数据进行格式化
-    $('.purchase-quantity input').blur(function () {
-        var val = $(this).val();
-        if (isNaN(val)) {
-            $(this).val(1);
-        } else if (val < 1 || val > 999) {
-            $.alert('数量只能在1-999之间')
-            $(this).val(1);
-        }
+    function purchaseQuantity() {
+        var conut = '';
+        // 对输入框中的数据进行格式化
+        $('.purchase-quantity input').blur(function () {
+            var val = $(this).val();
+            if (isNaN(val)) {
+                $(this).val(1);
+            } else if (val < 1 || val > 999) {
+                $.alert('数量只能在1-999之间')
+                $(this).val(1);
+            }
 
-    })
-    $(".purchase-quantity .btn-sub").on('click', function () {
-        var val = $('.purchase-quantity input').val();
-        if (val == 1) {
-            return;
-        }
-        val = val - 1;
-        $('.purchase-quantity input').val(val);
-    });
-    $(".purchase-quantity .btn-plus").on('click', function () {
-        var val = $('.purchase-quantity input').val();
-        if (val == 999) {
-            return;
-        }
-        val = parseInt(val) + 1;
-        $('.purchase-quantity input').val(val);
-    });
-}
+        })
+        $(".purchase-quantity .btn-sub").on('click', function () {
+            var val = $('.purchase-quantity input').val();
+            if (val == 1) {
+                return;
+            }
+            val = val - 1;
+            $('.purchase-quantity input').val(val);
+        });
+        $(".purchase-quantity .btn-plus").on('click', function () {
+            var val = $('.purchase-quantity input').val();
+            if (val == 999) {
+                return;
+            }
+            val = parseInt(val) + 1;
+            $('.purchase-quantity input').val(val);
+        });
+    }
 
 // 悬浮框
-function suspensionBox() {
-    var nav = $(".goods-table .table-title").children('ul'); //得到导航对象
-    $(window).scroll(function () {
-        if ($(document).scrollTop() >= 570) {
-            nav.addClass('fiexd');
-            nav.fadeIn();
-        } else {
-            nav.removeClass('fiexd');
-        }
-    })
-}
-suspensionBox();
-switchHover();
-purchaseQuantity();
+    function suspensionBox() {
+        var nav = $(".goods-table .table-title").children('ul'); //得到导航对象
+        $(window).scroll(function () {
+            if ($(document).scrollTop() >= 570) {
+                nav.addClass('fiexd');
+                nav.fadeIn();
+            } else {
+                nav.removeClass('fiexd');
+            }
+        })
+    }
+    suspensionBox();
+    switchHover();
+    purchaseQuantity();
+})
+
 
 // $.getUrlParam = function (key) {
 //     var reg = new RegExp("(^|&)" + key + "=([^&]*)(&|$)");
 //     var result = window.location.search.substr(1).match(reg);
 //     return result ? decodeURIComponent(result[2]) : null;
 // };
+
+var priceT = (JshopPrice / 100).toFixed(2)
+$('#J_goodsPrice').html(priceT);
+
 var id = $.getUrlParam('id');
 var format = $.getUrlParam('format');
 var txts = null;
