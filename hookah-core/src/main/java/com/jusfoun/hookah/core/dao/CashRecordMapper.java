5e4f72b7fbe48fe543f9d644bf05b2b97ea81fba
邓许 <mail.deng@qq.com>
2017-06-30 09:44:31 +0800

    添加流水表 流水记录

---
+++ hookah-core/src/main/java/com/jusfoun/hookah/core/dao/CashRecordMapper.java
@@ -0,0 +1,8 @@
+package com.jusfoun.hookah.core.dao;
+
+import com.jusfoun.hookah.core.domain.CashRecord;
+import com.jusfoun.hookah.core.generic.GenericDao;
+
+public interface CashRecordMapper extends GenericDao<CashRecord> {
+
+}
