1b4327431e4b283f6577c84192329cbfeea6a9d9
lutong <lt@gbdex.com>
2017-07-05 17:10:01 +0800

    签到

---
+++ hookah-rpc-api/src/main/java/com/jusfoun/hookah/rpc/api/PaySignService.java
@@ -0,0 +1,14 @@
+package com.jusfoun.hookah.rpc.api;
+
+import com.jusfoun.hookah.core.domain.PaySign;
+import com.jusfoun.hookah.core.generic.GenericService;
+
+/**
+ * Created by lt on 2017/7/5.
+ */
+public interface PaySignService extends GenericService<PaySign,String> {
+
+    void sendMarketLogin(PaySign paySign);
+
+    void sendMarketLogout(PaySign paySign);
+}
