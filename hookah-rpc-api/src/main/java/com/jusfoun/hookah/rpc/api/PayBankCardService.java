20947b4a269a218d03a30b6c3470b4ae5b57f891
xuyongmao <xym@jusfoun.com>
2017-07-05 10:41:03 +0800

    PayBankCard

---
+++ hookah-rpc-api/src/main/java/com/jusfoun/hookah/rpc/api/PayBankCardService.java
@@ -0,0 +1,10 @@
+package com.jusfoun.hookah.rpc.api;
+
+import com.jusfoun.hookah.core.domain.PayBankCard;
+import com.jusfoun.hookah.core.domain.User;
+import com.jusfoun.hookah.core.generic.GenericService;
+
+
+public interface PayBankCardService extends GenericService<PayBankCard, String> {
+
+}
