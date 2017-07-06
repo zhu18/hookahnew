783fcda8fdfab9b13e56462e01735a4ae3f641ff
niedefang <ndf@gbdex.com>
2017-07-06 13:53:27 +0800

    客户信息同步功能

---
+++ hookah-pay/src/main/java/com.jusfoun.hookah.pay/controller/SyncCustomerInfoController.java
@@ -0,0 +1,19 @@
+package com.jusfoun.hookah.pay.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by ndf on 2017/7/6.
+ */
+@Controller
+@RequestMapping("syncCustomerInfo")
+public class SyncCustomerInfoController {
+    //@Resource
+    //private SyncCustomerInfoService syncCustomerInfoService;
+
+    public Object syncCustomerInfo(){
+
+        return null;
+    }
+}
