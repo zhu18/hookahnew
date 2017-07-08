aa5267285a5aa6fa201b65b056cc725e163964c9
zhaoshuai <zhaoshuai@gbdex.com>
2017-07-08 14:19:00 +0800

    客户同步信息

---
+++ hookah-pay/src/main/java/com.jusfoun.hookah.pay/util/SyncCustomerInfoFlag.java
@@ -0,0 +1,14 @@
+package com.jusfoun.hookah.pay.util;
+
+/**
+ * Created by computer on 2017/7/7.
+ */
+public class SyncCustomerInfoFlag {
+    //客户同步信息
+    //未同步
+    public static final byte  NOT_SYNCHRONIZED = 0;
+    //同步中
+    public static final byte  SYNCHRONIZATION = 1;
+    //已同步
+    public static final byte  SYNCHRONIZED = 2;
+}
