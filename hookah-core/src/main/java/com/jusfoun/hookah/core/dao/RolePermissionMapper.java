d516adbad2a4e929d1c3f5f13e686f403f997ac9
黄磊 <elvis.huanglei@gmail.com>
2017-07-10 16:49:42 +0800

    角色，权限实体类与myBatis Mapper

--- hookah-core/src/main/java/com/jusfoun/hookah/core/dao/RolePermissionMapper.java
+++ hookah-core/src/main/java/com/jusfoun/hookah/core/dao/RolePermissionMapper.java
@@ -2,6 +2,9 @@ package com.jusfoun.hookah.core.dao;
 
 import com.jusfoun.hookah.core.domain.RolePermission;
 import com.jusfoun.hookah.core.generic.GenericDao;
+import org.apache.ibatis.annotations.Param;
 
 public interface RolePermissionMapper extends GenericDao<RolePermission> {
+
+    int deletePermissionsByRoleId(@Param("roleId") String roleId);
 }
