278cf838c32902b27072deac4112a5e78817d0ae
kongyang <2268560760@qq.com>
2017-07-13 08:58:58 +0800

    添加消息模板

--- hookah-console-angular/src/apps/message/MessageController.js
+++ hookah-console-angular/src/apps/message/MessageController.js
@@ -88,6 +88,26 @@ class MessageController {
             $scope.systemSearch();
         }
 
+        if ($state.$current.name == "message.template.add") {
+
+            //获取消息事件类型列表
+            var promise = $http({
+                method: 'POST',
+                url: $rootScope.site.apiServer + "/api/message/eventType/all",
+            });
+            promise.then(function (res, status, config, headers) {
+                $rootScope.loadingState = false;
+                console.log("获取数据:" + res.data);
+                if (res.data.code == 1) {
+                    console.log(res.data.data);
+                    $scope.eventTypes = res.data.data;
+                    // $scope.eventType = $scope.eventTypes[0].code;
+                    $scope.eventType = "-1";
+                }
+            });
+
+        }
+
         if($state.$current.name == "message.email.search"){
             //消息是否已读
             $scope.messageIsReads = [{id: -1, name: "全部"}, {id: 0, name: "未读"}, {id: 1, name: "已读"}];
