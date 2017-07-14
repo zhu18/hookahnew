497b96bc3106c27bdb531139faa5a9fd5f237ec4
zhaoshuai <zhaoshuai@gbdex.com>
2017-07-14 09:54:44 +0800

    优化

--- hookah-website/src/main/java/com/jusfoun/hookah/webiste/controller/CertificationController.java
+++ hookah-website/src/main/java/com/jusfoun/hookah/webiste/controller/CertificationController.java
@@ -9,6 +9,8 @@ import com.jusfoun.hookah.rpc.api.OrganizationService;
 import com.jusfoun.hookah.rpc.api.UserCheckService;
 import com.jusfoun.hookah.rpc.api.UserService;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,7 +30,9 @@ import java.util.Map;
 
 @Controller
 @RequestMapping(value = "/regInfo")
-public class CertificationController {
+public class CertificationController{
+    private static final Logger logger = LoggerFactory.getLogger(SupplierController.class);
+
     @Resource
     OrganizationService organizationService;
 
@@ -42,16 +46,15 @@ public class CertificationController {
     @ResponseBody
     @RequestMapping(value = "/verifiedInfo",method = RequestMethod.GET)
     public ReturnData verifiedInfo(String userId) {
-
+        Map<String, Object> map = new HashMap<>(6);
         User user = userService.selectById(userId);
-        List<Condition> filters = new ArrayList();
-        filters.add(Condition.eq("userId", userId));
-        UserCheck userCheck = userCheckService.selectOne(filters);
-        Map map = new HashMap();
         if(StringUtils.isNotBlank(user.getOrgId())) {
             Organization organization = organizationService.selectById(user.getOrgId());//根据用户查询认证信息
             //机构中 已认证状态 与 认证不通过状态
             if (organization.getIsAuth() == 2 || organization.getIsAuth() == 3) {
+                List<Condition> filters = new ArrayList();
+                filters.add(Condition.eq("userId", userId));
+                UserCheck userCheck = userCheckService.selectOne(filters);
                 map.put("userType", userCheck.getUserType());
                 map.put("isAuth", organization.getIsAuth());
                 map.put("checkContent", userCheck.getCheckContent());
@@ -77,12 +80,16 @@ public class CertificationController {
     @ResponseBody
     @RequestMapping(value = "/updateVerifiedInfo",method = RequestMethod.GET)
     public ReturnData updateVerifiedInfo(Organization org) {
-        try {
-            organizationService.updateByIdSelective(org);
-        }catch (Exception e){
-            ReturnData.error("修改失败");
-            e.printStackTrace();
+        if (StringUtils.isBlank(org.getOrgId())) {
+            return ReturnData.invalidParameters("参数orgId不可为空");
         }
-        return ReturnData.success();
+        try {
+            //organizationService.updateByIdSelective(org);
+            organizationService.updateById(org);
+            return ReturnData.success();
+        }catch (Exception e){
+            logger.error("修改错误", e);
+            return ReturnData.error("系统异常");
+        }
     }
 }
