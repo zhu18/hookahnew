25b3bb297e8c30e400779b895c5e3552952bca68
xuyongmao <xym@jusfoun.com>
2017-07-18 17:55:07 +0800

    fundRecordService

--- hookah-rpc-api/src/main/java/com/jusfoun/hookah/rpc/api/FundRecordService.java
+++ hookah-rpc-api/src/main/java/com/jusfoun/hookah/rpc/api/FundRecordService.java
@@ -1,13 +1,19 @@
 package com.jusfoun.hookah.rpc.api;
 
 import com.jusfoun.hookah.core.common.Pagination;
+import com.jusfoun.hookah.core.domain.OrderInfo;
 import com.jusfoun.hookah.core.domain.PayTradeRecord;
+import com.jusfoun.hookah.core.domain.mongo.MgOrderGoods;
 import com.jusfoun.hookah.core.domain.vo.OrderInfoVo;
+import com.jusfoun.hookah.core.domain.vo.PayVo;
+import com.jusfoun.hookah.core.exception.HookahException;
 import com.jusfoun.hookah.core.generic.Condition;
 import com.jusfoun.hookah.core.generic.GenericService;
 import com.jusfoun.hookah.core.generic.OrderBy;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 public interface FundRecordService extends GenericService<PayTradeRecord, String> {
 
