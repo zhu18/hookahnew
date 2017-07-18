96b77301236528d79006465dd4dae173330a1c65
邓许 <mail.deng@qq.com>
2017-07-18 08:57:35 +0800

    提现申请 查询 审批

---
+++ hookah-console-server/src/main/java/com/jusfoun/hookah/console/server/api/withdraw/WithdrawRecordController.java
@@ -0,0 +1,173 @@
+package com.jusfoun.hookah.console.server.api.withdraw;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.jusfoun.hookah.console.server.controller.BaseController;
+import com.jusfoun.hookah.core.common.Pagination;
+import com.jusfoun.hookah.core.constants.HookahConstants;
+import com.jusfoun.hookah.core.domain.PayAccount;
+import com.jusfoun.hookah.core.domain.WithdrawRecord;
+import com.jusfoun.hookah.core.domain.vo.WithdrawVo;
+import com.jusfoun.hookah.core.generic.Condition;
+import com.jusfoun.hookah.core.utils.ExceptionConst;
+import com.jusfoun.hookah.core.utils.ReturnData;
+import com.jusfoun.hookah.core.utils.StringUtils;
+import com.jusfoun.hookah.rpc.api.PayAccountService;
+import com.jusfoun.hookah.rpc.api.WithdrawRecordService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * 后台提现
+ */
+
+@RestController
+@RequestMapping("/api/withdrawRecord")
+public class WithdrawRecordController extends BaseController {
+
+    @Resource
+    WithdrawRecordService withdrawRecordService;
+
+    @Resource
+    PayAccountService payAccountService;
+
+    /**
+     * 查询所有申请列表
+     */
+    @RequestMapping("/getList")
+    public ReturnData getList(String startDate, String endDate,
+                              String currentPage, String pageSize,
+                              String checkStatus, String orgName
+    ) {
+
+        ReturnData returnData = new ReturnData<>();
+        returnData.setCode(ExceptionConst.Success);
+        Pagination<WithdrawRecord> pagination = new Pagination<>();
+        PageInfo<WithdrawRecord> page = new PageInfo<>();
+        try {
+
+            int pageNumberNew = HookahConstants.PAGE_NUM;
+            if (StringUtils.isNotBlank(currentPage)) {
+                pageNumberNew = Integer.parseInt(currentPage);
+            }
+
+            int pageSizeNew = HookahConstants.PAGE_SIZE;
+            if (StringUtils.isNotBlank(pageSize)) {
+                pageSizeNew = Integer.parseInt(pageSize);
+            }
+
+            PageHelper.startPage(pageNumberNew, pageSizeNew);   //pageNum为第几页，pageSize为每页数量
+            List<WithdrawRecord> list = withdrawRecordService.getListForPage(startDate, endDate, checkStatus, orgName);
+            page = new PageInfo<WithdrawRecord>(list);
+
+            pagination.setTotalItems(page.getTotal());
+            pagination.setPageSize(pageSizeNew);
+            pagination.setCurrentPage(pageNumberNew);
+            pagination.setList(page.getList());
+
+            returnData.setData(pagination);
+
+        } catch (Exception e) {
+            returnData.setCode(ExceptionConst.Failed);
+            returnData.setMessage("系统出错，请联系管理员！");
+            e.printStackTrace();
+        }
+        return returnData;
+    }
+
+    /**
+     * 根据id查看审核记录
+     * @param id
+     * @return
+     */
+    @RequestMapping("/getOneById")
+    public ReturnData getOne(@RequestParam("id") Long id){
+
+        ReturnData returnData = new ReturnData<>();
+        returnData.setCode(ExceptionConst.Success);
+        try {
+
+            WithdrawRecord record = withdrawRecordService.selectById(id);
+            if(record == null){
+                returnData.setCode(ExceptionConst.Failed);
+                returnData.setMessage("未查询到此条提现记录，请联系管理员！");
+                return returnData;
+            }
+
+            List<Condition> filters = new ArrayList();
+            filters.add(Condition.eq("userId", record.getUserId()));
+            PayAccount payAccount = payAccountService.selectOne(filters);
+            if(payAccount == null){
+                returnData.setCode(ExceptionConst.Failed);
+                returnData.setMessage("未查询到账户信息，请联系管理员！");
+                return returnData;
+            }
+            WithdrawVo withdrawVo = new WithdrawVo();
+            BeanUtils.copyProperties(record, withdrawVo);
+            withdrawVo.setBalance(payAccount.getBalance());
+            withdrawVo.setUseBalance(payAccount.getUseBalance());
+
+            returnData.setData(withdrawVo);
+
+        } catch (Exception e) {
+            returnData.setCode(ExceptionConst.Failed);
+            returnData.setMessage("系统出错，请联系管理员！");
+            e.printStackTrace();
+        }
+        return returnData;
+    }
+
+    /**
+     * 根据id审核提现记录
+     * @param id
+     * @return
+     */
+    @RequestMapping("/checkOne")
+    public ReturnData checkOne(
+            @RequestParam("id") Long id,
+            @RequestParam("checkStatus") byte checkStatus,
+            String checkMsg
+    ){
+
+        ReturnData returnData = new ReturnData<>();
+        returnData.setCode(ExceptionConst.Success);
+        try {
+
+            if(checkStatus == HookahConstants.WithdrawStatus.checkFail.code && !StringUtils.isNotBlank(checkMsg)){
+                returnData.setCode(ExceptionConst.Failed);
+                returnData.setMessage("提现审核失败，请填写失败原因！");
+                return returnData;
+            }
+
+            WithdrawRecord record = new WithdrawRecord();
+            record.setId(id);
+            record.setCheckStatus(checkStatus);
+            record.setCheckMsg(checkMsg);
+            record.setChecckTime(new Date());
+            record.setCheckOperator(getCurrentUser().getUserName());
+            int n = withdrawRecordService.updateByIdSelective(record);
+            if(n == 1){
+                returnData.setCode(ExceptionConst.Success);
+                returnData.setMessage("审核成功！");
+            }else{
+                returnData.setCode(ExceptionConst.Failed);
+                returnData.setMessage("审核失败！");
+            }
+
+        } catch (Exception e) {
+            returnData.setCode(ExceptionConst.Failed);
+            returnData.setMessage("系统出错，请联系管理员！");
+            e.printStackTrace();
+        }
+        return returnData;
+    }
+
+}
+
