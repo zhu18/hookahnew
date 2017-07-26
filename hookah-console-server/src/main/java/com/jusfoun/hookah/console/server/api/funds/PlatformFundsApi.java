dbb35ba92c9ad387b6c8a511e48b8b9de7df301e
邓许 <mail.deng@qq.com>
2017-07-26 11:26:19 +0800

    分页参数调整

--- hookah-console-server/src/main/java/com/jusfoun/hookah/console/server/api/funds/PlatformFundsApi.java
+++ hookah-console-server/src/main/java/com/jusfoun/hookah/console/server/api/funds/PlatformFundsApi.java
@@ -237,7 +237,7 @@ public class PlatformFundsApi extends BaseController{
                     listVo.add(vo);
                 });
             }
-            pagination.setTotalItems(page.getTotalItems());
+            pagination.setTotalPage(page.getTotalPage());
             pagination.setPageSize(pageSizeNew);
             pagination.setCurrentPage(pageNumberNew);
             pagination.setList(listVo);
