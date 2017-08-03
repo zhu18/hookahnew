ae2a8fa99e249cb297d977db5d66480f5c31985c
niedefang <ndf@gbdex.com>
2017-08-03 14:10:58 +0800

    修复系统用户新增时角色列表显示不全问题，及添加表单验证

--- hookah-console-angular/src/apps/account/AccountController.js
+++ hookah-console-angular/src/apps/account/AccountController.js
@@ -86,7 +86,9 @@ class AccountController {
       var promise = $http({
         method: 'GET',
         url: $rootScope.site.apiServer + "/api/role/role_all",
-        data: ""
+        params: {
+            pageSize: 1000
+        }
       });
       promise.then(function (res, status, config, headers) {
         $rootScope.roles = res.data.data.list;
