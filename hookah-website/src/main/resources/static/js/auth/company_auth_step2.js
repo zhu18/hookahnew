951cb0987b7f54bf8d611e6929a4bb62980442e4
lushanshan <409071865@qq.com>
2017-08-08 16:44:13 +0800

    fix 未认证情况的 立即认证

--- hookah-website/src/main/resources/static/js/auth/company_auth_step2.js
+++ hookah-website/src/main/resources/static/js/auth/company_auth_step2.js
@@ -103,11 +103,11 @@ $.ajax({
    "officeAddress":$("input[name='workAddress']").val(),//详细地址
    "orgPhone":$("input[name='tel']").val(),//联系电话
    "taxCode":$("input[name='taxRegCertificate']").val(),//税务登记编号
-   "taxPath":$("input[name='taxPath']").val(),//税务登记存放路径
+   "taxPath":$("#taxPathSrc").val(),//税务登记存放路径
    "licenseCode":$("input[name='businessLicence']").val(),//营业执照编号
-   "licensePath":$("input[name='licensePath']").val(),//营业执照存放路径
+   "licensePath": $("#licensePathSrc").val(),//营业执照存放路径
    "certificateCode":$("input[name='certificateCode']").val(),//信用代码
-   "certifictePath":$("input[name='certifictePath']").val(),//企业代码存放路径
+   "certifictePath":$("#certifictePathSrc").val(),//企业代码存放路径
    "isSupplier":$("input[name='fruit']:checked").val()?$("input[name='fruit']:checked").val():"0"//是否成功供应商
    },
    type:"post",
--- hookah-website/src/main/resources/static/js/usercenter/userInfo/fundmanage.js
+++ hookah-website/src/main/resources/static/js/usercenter/userInfo/fundmanage.js
@@ -11,63 +11,72 @@ function render() {
         cache:false,
         type:'get',
         success:function (data) {
-            var list=data.data.bank;
-            $.ajax({
-                url:host.website+'/payBankCard/userFunds',
-                data:{},
-                type:'get',
-                success:function (data) {
-                    $(".account-funds-content-left .money").html("￥"+(data.data.useBalance / 100).toFixed(2));
-                    $(".freeze").html("￥"+(data.data.freeze / 100).toFixed(2));
-                    $(".useBalance").html("￥"+(data.data.balance / 100).toFixed(2));
-                    for(var i=0;i<list.length;i++){
-                        if(list[i].id==data.data.bankId){
-                            data.data.bankId=list[i].bankName;
-                        }
-                    }
-                    var html='';
-                    if(data.data.bindFlag=="0"){
-                        html +='<div class="show-card">';
-                        html +='<h4 class="card-name">'+data.data.bankId+'</h4><p>';
-                        html +='<span class="margin-right-5">银行卡号:</span>';
-                        html +='<span class="card-number">'+data.data.cardCode+'</span></p><p>';
-                        html +='<span class="margin-right-5">账户名:</span><span class="account-name">'+data.data.cardOwner+'</span></p><a href="javascript:void(0)" class="delete">删除</a></div>';
-                    }else {
-                        html +='<a href="/usercenter/bindBankCard" class="add-card">';
-                        html +='<p class="Plus margin-top-20">+</p>';
-                        html +='<p>添加银行</p>';
-                        $(".operation-btn").append('<span class="color-red margin-left-10">您还未绑定银行卡，<a href="/usercenter/bindBankCard" class="color-red">立即绑定</a></span>');
+            if(data.code == "1"){
+                var list=data.data.bank;
+                $.ajax({
+                    url:host.website+'/payBankCard/userFunds',
+                    data:{},
+                    type:'get',
+                    success:function (data) {
 
-                    }
-                    $(".bank-card-content").html(html);
-                    $(".withdrawals-btn").on("click",function () {
-                         if(data.data.bindFlag=="0"){
-                             window.location.href= host.website+'/withdrawRecord/getUserInfo';
-                         }else {
-                             $.alert("请先绑定银行卡！")
-                         }
-                    })
-                    // 删除银行卡
-                    $(".bank-card-content .delete").on("click",function () {
-                        $.alert('您确定要删除已绑定的银行卡吗？',true,function(){
-                            $.ajax({
-                                url:host.website+'/payBankCard/updateBankInfo',
-                                data:{
+                        $(".account-funds-content-left .money").html("￥"+(data.data.useBalance / 100).toFixed(2));
+                        $(".freeze").html("￥"+(data.data.frozenBalance / 100).toFixed(2));
+                        $(".useBalance").html("￥"+(data.data.balance / 100).toFixed(2));
 
-                                },
-                                type:'get',
-                                success:function (data) {
-                                    if (data.code=="1"){
-                                        $.alert('解绑银行卡成功！');
-                                         render()
-                                    }
+                        if(data.data.userType == 4){
+                            for(var i=0;i<list.length;i++){
+                                if(list[i].id==data.data.bankId){
+                                    data.data.bankId=list[i].bankName;
                                 }
-                            });
-                        });
+                            }
+                            var html='';
+                            if(data.data.bindFlag=="0"){
+                                html +='<div class="show-card">';
+                                html +='<h4 class="card-name">'+data.data.bankId+'</h4><p>';
+                                html +='<span class="margin-right-5">银行卡号:</span>';
+                                html +='<span class="card-number">'+data.data.cardCode+'</span></p><p>';
+                                html +='<span class="margin-right-5">账户名:</span><span class="account-name">'+data.data.cardOwner+'</span></p><a href="javascript:void(0)" class="delete">删除</a></div>';
+                            }else {
+                                html +='<a href="/usercenter/bindBankCard" class="add-card">';
+                                html +='<p class="Plus margin-top-20">+</p>';
+                                html +='<p>添加银行</p>';
+                                $(".operation-btn").append('<span class="color-red margin-left-10">您还未绑定银行卡，<a href="/usercenter/bindBankCard" class="color-red">立即绑定</a></span>');
 
-                    })
-                }
-            });
+                            }
+                            $(".bank-card-content").html(html);
+                            $(".withdrawals-btn").on("click",function () {
+                                if(data.data.bindFlag=="0"){
+                                    window.location.href= host.website+'/withdrawRecord/getUserInfo';
+                                }else {
+                                    $.alert("请先绑定银行卡！")
+                                }
+                            })
+                            // 删除银行卡
+                            $(".bank-card-content .delete").on("click",function () {
+                                $.alert('您确定要删除已绑定的银行卡吗？',true,function(){
+                                    $.ajax({
+                                        url:host.website+'/payBankCard/updateBankInfo',
+                                        data:{
+
+                                        },
+                                        type:'get',
+                                        success:function (data) {
+                                            if (data.code=="1"){
+                                                $.alert('解绑银行卡成功！');
+                                                render()
+                                            }
+                                        }
+                                    });
+                                });
+
+                            })
+                        }else {$(".bank-card").hide()}
+
+                    }
+                });
+            }else {
+                $(".bank-card").hide()
+            }
         }
     });
 
