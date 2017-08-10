2a463fe2386ed656f357868a4137e6143642f4f3
赵书军 <446888014@qq.com>
2017-08-10 18:02:47 +0800

    充值前提示充值金额

--- hookah-console-angular/src/apps/user/rechargeController.js
+++ hookah-console-angular/src/apps/user/rechargeController.js
@@ -3,32 +3,39 @@
  */
 class rechargeController {
   constructor($scope, $rootScope, $state, $http, $stateParams, growl) {
-    $scope.userBaseInfo=$stateParams.item;
+    $scope.userBaseInfo = $stateParams.item;
     console.log($scope.userBaseInfo);
 
     $scope.recharge = function () {
-      if( $scope.money <= 0){
-        alert("充值金额必须大于 0 ！")
+      if ($scope.money <= 0) {
+        alert("充值金额必须大于 0 ！");
+        return;
       }
-      var promise = $http({
-        method: 'POST',
-        url: $rootScope.site.apiServer + "/api/payAccountBg/rechargeByHand",
-        params: {
-          userId: $scope.userBaseInfo.userId  ,
-          money:  $scope.money ,
-        }
-      });
-      promise.then(function (res, status, config, headers) {
+      var modalInstance = $rootScope.openConfirmDialogModal("您要充值的金额是：￥" + '<span style="font-weight: bold;color: red;">' + $scope.money + '</span>');
+      modalInstance.result.then(function () {
+        var promise = $http({
+          method: 'POST',
+          url: $rootScope.site.apiServer + "/api/payAccountBg/rechargeByHand",
+          params: {
+            userId: $scope.userBaseInfo.userId,
+            money: $scope.money,
+          }
+        });
+        promise.then(function (res, status, config, headers) {
 
-        if (res.data.code == "1") {
-          alert(res.data.message);
-          //  $state.go('user.search');
-        } else {
-          alert(res.data.message);
-        }
+          if (res.data.code == "1") {
+            alert(res.data.message);
+            //  $state.go('user.search');
+          } else {
+            alert(res.data.message);
+          }
+        });
+
+      }, function () {
+        // $log.info('Modal dismissed at: ' + new Date());
       });
+
     };
-
 
 
     $scope.back = function () {
