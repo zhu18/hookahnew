af1e19595a9178b82c8e2c8a0c41b8550dd2a18d
陈涛平 <ctp971412157@qq.com>
2017-09-18 01:23:26 +0800

    众包项目搭建

---
+++ hookah-crowd-website/src/main/java/com/jusfoun/hookah/crowd/shiro/OAuth2Token.java
@@ -0,0 +1,39 @@
+package com.jusfoun.hookah.crowd.shiro;
+
+import org.apache.shiro.authc.AuthenticationToken;
+
+/**
+ * @author huang lei
+ * @date 2017/3/27 下午15:26
+ * @desc
+ */
+public class OAuth2Token implements AuthenticationToken {
+
+    public OAuth2Token(String authCode) {
+        this.authCode = authCode;
+    }
+
+    private String authCode;
+    private String principal;
+
+    public String getAuthCode() {
+        return authCode;
+    }
+
+    public void setAuthCode(String authCode) {
+        this.authCode = authCode;
+    }
+
+    public String getPrincipal() {
+        return principal;
+    }
+
+    public void setPrincipal(String principal) {
+        this.principal = principal;
+    }
+
+    @Override
+    public Object getCredentials() {
+        return authCode;
+    }
+}
