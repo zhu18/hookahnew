5956673ede36885c5018ddaf4e7d431d6f253af8
lutong <lt@gbdex.com>
2017-09-18 18:03:12 +0800

    实体

--- hookah-core/src/main/java/com/jusfoun/hookah/core/domain/ZbServiceProviderPerson.java
+++ hookah-core/src/main/java/com/jusfoun/hookah/core/domain/ZbServiceProviderPerson.java
@@ -1,11 +1,12 @@
 package com.jusfoun.hookah.core.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.jusfoun.hookah.core.generic.GenericModel;
 
 import javax.persistence.Id;
 import java.util.Date;
 
-public class ZbServiceProviderPerson {
+public class ZbServiceProviderPerson extends GenericModel {
   @Id
   private Long id;
   private String userId;
