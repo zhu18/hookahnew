2790ef01629f0953837df68aa8c38f46cd135c0b
zhaoshuai <zhaoshuai@gbdex.com>
2017-09-18 17:21:45 +0800

    数据众包实体类

---
+++ hookah-core/src/main/java/com/jusfoun/hookah/core/domain/Zb_requirement_apply.java
@@ -0,0 +1,65 @@
+package com.jusfoun.hookah.core.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.persistence.Id;
+import java.util.Date;
+
+public class Zb_requirement_apply {
+  @Id
+  private Long id;
+  private Long requirementId;
+  private String userId;
+  private String applyContent;
+  private Long status;
+  @JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
+  private Date addTime;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Long getRequirementId() {
+    return requirementId;
+  }
+
+  public void setRequirementId(Long requirementId) {
+    this.requirementId = requirementId;
+  }
+
+  public String getUserId() {
+    return userId;
+  }
+
+  public void setUserId(String userId) {
+    this.userId = userId;
+  }
+
+  public String getApplyContent() {
+    return applyContent;
+  }
+
+  public void setApplyContent(String applyContent) {
+    this.applyContent = applyContent;
+  }
+
+  public Long getStatus() {
+    return status;
+  }
+
+  public void setStatus(Long status) {
+    this.status = status;
+  }
+
+  public Date getAddTime() {
+    return addTime;
+  }
+
+  public void setAddTime(Date addTime) {
+    this.addTime = addTime;
+  }
+}
---
+++ hookah-core/src/main/java/com/jusfoun/hookah/core/domain/Zb_user.java
@@ -0,0 +1,55 @@
+package com.jusfoun.hookah.core.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.util.Date;
+
+public class Zb_user {
+  private Long userId;
+  private String userDesc;
+  private Long userStatus;
+  @JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
+  private Date addTime;
+  @JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
+  private Date updateTime;
+
+  public Long getUserId() {
+    return userId;
+  }
+
+  public void setUserId(Long userId) {
+    this.userId = userId;
+  }
+
+  public String getUserDesc() {
+    return userDesc;
+  }
+
+  public void setUserDesc(String userDesc) {
+    this.userDesc = userDesc;
+  }
+
+  public Long getUserStatus() {
+    return userStatus;
+  }
+
+  public void setUserStatus(Long userStatus) {
+    this.userStatus = userStatus;
+  }
+
+  public Date getAddTime() {
+    return addTime;
+  }
+
+  public void setAddTime(Date addTime) {
+    this.addTime = addTime;
+  }
+
+  public Date getUpdateTime() {
+    return updateTime;
+  }
+
+  public void setUpdateTime(Date updateTime) {
+    this.updateTime = updateTime;
+  }
+}
