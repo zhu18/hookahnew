183f12525a095c30bdbad978c91bea86df5f0a8f
曹容山 <crs@gbdex.com>
2017-09-19 11:06:53 +0800

    代码迁移

--- hookah-crowd-website/src/main/java/com/jusfoun/hookah/crowd/controller/RequireController.java
+++ hookah-crowd-website/src/main/java/com/jusfoun/hookah/crowd/controller/RequireController.java
@@ -44,41 +44,12 @@ public class RequireController extends BaseController{
     @RequestMapping("/allRequirement")
     @ResponseBody
     public ReturnData AllRequirement(String currentPage, String pageSize, ZbRequirement zbRequirement) {
-        Pagination<ZbRequirement> page = new Pagination<>();
-        ReturnData returnData = new ReturnData<>();
-        returnData.setCode(ExceptionConst.Success);
         try {
-            List<Condition> filters = new ArrayList();
-            List<OrderBy> orderBys = new ArrayList();
-            orderBys.add(OrderBy.desc("addTime"));
-
-            if (StringUtils.isNotBlank(zbRequirement.getRequireSn())) {
-                filters.add(Condition.like("requireSn", zbRequirement.getRequireSn()));
-            }
-            if (StringUtils.isNotBlank(zbRequirement.getTitle())) {
-                filters.add(Condition.like("title", zbRequirement.getTitle()));
-            }
-            if (zbRequirement.getStatus() != null && zbRequirement.getStatus()!= -1) {
-                filters.add(Condition.eq("status", zbRequirement.getStatus()));
-            }
-
-            int pageNumberNew = HookahConstants.PAGE_NUM;
-            if (StringUtils.isNotBlank(currentPage)) {
-                pageNumberNew = Integer.parseInt(currentPage);
-            }
-
-            int pageSizeNew = HookahConstants.PAGE_SIZE;
-            if (StringUtils.isNotBlank(pageSize)) {
-                pageSizeNew = Integer.parseInt(pageSize);
-            }
-            page = zbRequireService.getListInPage(pageNumberNew, pageSizeNew, filters, orderBys);
-            returnData.setData(page);
-        } catch (Exception e) {
-            returnData.setCode(ExceptionConst.Error);
-            returnData.setMessage(e.toString());
-            e.printStackTrace();
+                return zbRequireService.getAllRequirement(currentPage, pageSize, zbRequirement);
+        }catch (Exception e){
+                logger.error("需求查询失败", e);
+                return ReturnData.error("需求查询失败");
         }
-        return returnData;
     }
 
     /**
--- hookah-crowd-website/src/main/java/com/jusfoun/hookah/crowd/service/impl/ZbRequireServiceImpl.java
+++ hookah-crowd-website/src/main/java/com/jusfoun/hookah/crowd/service/impl/ZbRequireServiceImpl.java
@@ -1,12 +1,14 @@
 package com.jusfoun.hookah.crowd.service.impl;
 
 import com.jusfoun.hookah.core.common.Pagination;
+import com.jusfoun.hookah.core.constants.HookahConstants;
 import com.jusfoun.hookah.core.dao.zb.ZbRequirementMapper;
 import com.jusfoun.hookah.core.domain.zb.ZbRequirement;
 import com.jusfoun.hookah.core.exception.HookahException;
 import com.jusfoun.hookah.core.generic.Condition;
 import com.jusfoun.hookah.core.generic.GenericServiceImpl;
 import com.jusfoun.hookah.core.generic.OrderBy;
+import com.jusfoun.hookah.core.utils.ExceptionConst;
 import com.jusfoun.hookah.core.utils.ReturnData;
 import com.jusfoun.hookah.crowd.service.ZbRequireService;
 import org.apache.commons.lang3.StringUtils;
@@ -46,6 +48,45 @@ public class ZbRequireServiceImpl extends GenericServiceImpl<ZbRequirement, Long
         orderBys.add(OrderBy.desc("addTime"));
         Pagination<ZbRequirement> list= getListInPage(pageNum, pageSize,filter,orderBys);
         return ReturnData.success(list);
+    }
+
+    @Override
+    public ReturnData<ZbRequirement> getAllRequirement(String currentPage, String pageSize, ZbRequirement zbRequirement) {
+        Pagination<ZbRequirement> page = new Pagination<>();
+        ReturnData returnData = new ReturnData<>();
+        returnData.setCode(ExceptionConst.Success);
+        try {
+            List<Condition> filters = new ArrayList();
+            List<OrderBy> orderBys = new ArrayList();
+            orderBys.add(OrderBy.desc("addTime"));
+
+            if (StringUtils.isNotBlank(zbRequirement.getRequireSn())) {
+                filters.add(Condition.like("requireSn", zbRequirement.getRequireSn()));
+            }
+            if (StringUtils.isNotBlank(zbRequirement.getTitle())) {
+                filters.add(Condition.like("title", zbRequirement.getTitle()));
+            }
+            if (zbRequirement.getStatus() != null && zbRequirement.getStatus()!= -1) {
+                filters.add(Condition.eq("status", zbRequirement.getStatus()));
+            }
+
+            int pageNumberNew = HookahConstants.PAGE_NUM;
+            if (StringUtils.isNotBlank(currentPage)) {
+                pageNumberNew = Integer.parseInt(currentPage);
+            }
+
+            int pageSizeNew = HookahConstants.PAGE_SIZE;
+            if (StringUtils.isNotBlank(pageSize)) {
+                pageSizeNew = Integer.parseInt(pageSize);
+            }
+            page = getListInPage(pageNumberNew, pageSizeNew, filters, orderBys);
+            returnData.setData(page);
+        } catch (Exception e) {
+            returnData.setCode(ExceptionConst.Error);
+            returnData.setMessage(e.toString());
+            e.printStackTrace();
+        }
+        return returnData;
     }
 
 
