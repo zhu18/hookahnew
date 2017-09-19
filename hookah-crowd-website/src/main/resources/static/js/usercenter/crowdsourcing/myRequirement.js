241c7da6eb386562e6ae57367e151d3b1b402250
zsj_x <zhaoshujun>
2017-09-19 18:45:24 +0800

    众包路由

---
+++ hookah-crowd-website/src/main/resources/static/js/usercenter/crowdsourcing/myRequirement.js
@@ -0,0 +1,106 @@
+/**
+ * Created by Dajun on 2017-9-19.
+ */
+function loadPageData(data) { //渲染页面数据
+  console.log(data);
+  data = data.data.list;
+  var tempHtml = '\
+        <thead>\
+          <tr>\
+          <th>序号</th>\
+          <th>需求编号</th>\
+          <th>需求标题</th>\
+          <th>悬赏金额</th>\
+          <th>状态</th>\
+          <th>发布时间</th>\
+          <th>交付截止时间</th>\
+          <th>操作</th>\
+          </tr>\
+          </thead>\
+          <tbody>';
+  for (var i = 0; i < data.length; i++) {
+    let tempState='';
+    let tempEdit='';
+    switch(data[i].status)
+    {
+      case 1:
+        tempState='待审核';
+        tempEdit='<span class="signUp">修改</span><span class="signUp">删除</span>';
+        break;
+      case 2:
+        tempState='审核未通过';
+        tempEdit='<span class="signUp">修改</span><span class="signUp">删除</span>';
+
+        break;
+      case 3:
+        tempState='待托管赏金';
+        tempEdit='<span class="signUp">托管资金</span>';
+        break;
+      case 7:
+        tempState='待二次托管';
+        tempEdit='<span class="signUp">托管资金</span>';
+        break;
+      case 8:
+        tempState='工作中';
+        tempEdit='<span class="signUp">查看</span>';
+        break;
+      case 10:
+        tempState='待验收付款';
+        tempEdit='<span class="signUp">验收</span>';
+        break;
+      case 13:
+        tempState='待评价';
+        tempEdit='<span class="signUp">去评价</span>';
+        break;
+      case 14:
+        tempState='交易取消';
+        tempEdit='<span class="signUp">查看</span>';
+        break;
+      case 15:
+        tempState='交易完成';
+        tempEdit='<span class="signUp">查看</span>';
+        break;
+      case 16:
+        tempState='待退款';
+        tempEdit='<span class="signUp">查看</span>';
+        break;
+
+    }
+
+
+
+
+
+    tempHtml += '<tr>\
+          <td>'+i+'</td>\
+          <td>'+data[i].requireSn+'</td>\
+          <td>'+data[i].title+'</td>\
+          <td>'+data[i].rewardMoney+'</td>\
+          <td>'+tempState+'</td>\
+          <td>'+data[i].addTime+'</td>\
+          <td>'+data[i].deliveryDeadline+'</td>\
+          <td>'+tempEdit+'</td>\
+          </tr>'
+  }
+
+  tempHtml += '</tbody></table>'
+
+  $('.crowdsourcing-table').html(tempHtml)
+
+}
+
+$('#status li').on('click', function () {
+  $(this).addClass('active').siblings().removeClass('active');
+  $('#status').attr('value', $(this).attr('value'));
+  searchFn();
+});
+$('.searchBtn').on('click', function () {
+  searchFn();
+});
+function searchFn(){
+  dataParm.status = $('#status').attr('value');
+  dataParm.title = $('#title').val();
+  dataParm.requireSn = $('#requireSn').val();
+  console.log(dataParm);
+  goPage("1");
+}
