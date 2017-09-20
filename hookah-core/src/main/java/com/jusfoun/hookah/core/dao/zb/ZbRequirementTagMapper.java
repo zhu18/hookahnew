22a7c58737ad62281f19e6b02a1320962a3f1fb5
zhaoshuai <zhaoshuai@gbdex.com>
2017-09-20 14:27:14 +0800

    bug

--- hookah-core/src/main/java/com/jusfoun/hookah/core/dao/zb/ZbRequirementTagMapper.java
+++
@@ -1,8 +0,0 @@
-package com.jusfoun.hookah.core.dao.zb;
-
-import com.jusfoun.hookah.core.domain.zb.ZbRequirementTag;
-import com.jusfoun.hookah.core.generic.GenericDao;
-
-public interface ZbRequirementTagMapper extends GenericDao<ZbRequirementTag> {
-
-}
--- hookah-core/src/main/java/com/jusfoun/hookah/core/dao/zb/ZbTagMapper.java
+++
@@ -1,8 +0,0 @@
-package com.jusfoun.hookah.core.dao.zb;
-
-import com.jusfoun.hookah.core.domain.zb.ZbTag;
-import com.jusfoun.hookah.core.generic.GenericDao;
-
-public interface ZbTagMapper extends GenericDao<ZbTag> {
-
-}
