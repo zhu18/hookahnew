3c30cc4c9b328ff999189cde498680e90996f864
曹容山 <crs@gbdex.com>
2017-09-20 17:18:44 +0800

    需求类型名称

--- hookah-core/src/main/java/com/jusfoun/hookah/core/domain/zb/ZbRequirement.java
+++ hookah-core/src/main/java/com/jusfoun/hookah/core/domain/zb/ZbRequirement.java
@@ -151,9 +151,20 @@ public class ZbRequirement extends GenericModel {
      */
     private String updateOperator;
 
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    @Transient
+
+    private String typeName;
+
     @Transient
     private String remainTime;
-
     public String getRequiremetName() {
         return requiremetName;
     }
--- hookah-crowd-website/src/main/java/com/jusfoun/hookah/crowd/service/impl/ZbRequireServiceImpl.java
+++ hookah-crowd-website/src/main/java/com/jusfoun/hookah/crowd/service/impl/ZbRequireServiceImpl.java
@@ -2,7 +2,9 @@ package com.jusfoun.hookah.crowd.service.impl;
 
 import com.jusfoun.hookah.core.common.Pagination;
 import com.jusfoun.hookah.core.constants.HookahConstants;
+import com.jusfoun.hookah.core.dao.UserMapper;
 import com.jusfoun.hookah.core.dao.zb.ZbRequirementMapper;
+import com.jusfoun.hookah.core.dao.zb.ZbTypeMapper;
 import com.jusfoun.hookah.core.domain.User;
 import com.jusfoun.hookah.core.domain.zb.ZbRequirement;
 import com.jusfoun.hookah.core.domain.zb.ZbRequirementPageHelper;
@@ -27,6 +29,9 @@ public class ZbRequireServiceImpl extends GenericServiceImpl<ZbRequirement, Long
 
     @Resource
     private ZbRequirementMapper zbRequirementMapper;
+
+    @Resource
+    ZbTypeMapper zbTypeMapper;
 
     @Resource
     public void setDao(ZbRequirementMapper zbRequirementMapper) {
@@ -63,12 +68,12 @@ public class ZbRequireServiceImpl extends GenericServiceImpl<ZbRequirement, Long
             List<Condition> filters = new ArrayList();
             List<OrderBy> orderBys = new ArrayList();
             orderBys.add(OrderBy.desc("addTime"));
-
+             zbRequirement.setTypeName( zbTypeMapper.selectByPrimaryKey(zbRequirement.getType()).getTypeName());
             if (StringUtils.isNotBlank(zbRequirement.getRequireSn())) {
                 filters.add(Condition.like("requireSn", zbRequirement.getRequireSn()));
             }
             if (StringUtils.isNotBlank(zbRequirement.getTitle())) {
-                filters.add(Condition.like("title", zbRequirement.getTitle()));
+                filters.add(Condition.like(" title", zbRequirement.getTitle()));
             }
             if (zbRequirement.getStatus() != null && zbRequirement.getStatus()!= -1) {
                 filters.add(Condition.eq("status", zbRequirement.getStatus()));
