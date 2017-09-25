99444b377859f49c36fc2b8ebc7559a693082b08
邓许 <mail.deng@qq.com>
2017-09-25 09:16:22 +0800

    服务上认证

--- hookah-core/src/main/java/com/jusfoun/hookah/core/dao/zb/ZbRequirementMapper.java
+++ hookah-core/src/main/java/com/jusfoun/hookah/core/dao/zb/ZbRequirementMapper.java
@@ -2,7 +2,9 @@ package com.jusfoun.hookah.core.dao.zb;
 
 import com.jusfoun.hookah.core.domain.zb.ZbRequirement;
 import com.jusfoun.hookah.core.domain.zb.ZbRequirementPageHelper;
+import com.jusfoun.hookah.core.domain.zb.vo.ZbTradeRecord;
 import com.jusfoun.hookah.core.generic.GenericDao;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -13,4 +15,11 @@ public interface ZbRequirementMapper extends GenericDao<ZbRequirement> {
     int countRequirementList(ZbRequirementPageHelper helper);
 
     List<ZbRequirement> getRequirementList(ZbRequirementPageHelper helper);
+
+    List<ZbRequirement> selectListForPageByFilters(
+            @Param("requireSn") String requireSn,
+            @Param("title") String title,
+            @Param("zbStatus") Short[] zbStatus);
+
+    List<ZbTradeRecord> selectTradeRecodes(@Param("userId") String userId);
 }
--- hookah-crowd-website/src/main/java/com/jusfoun/hookah/crowd/service/MgZbProviderService.java
+++ hookah-crowd-website/src/main/java/com/jusfoun/hookah/crowd/service/MgZbProviderService.java
@@ -1,10 +1,22 @@
 package com.jusfoun.hookah.crowd.service;
 
 import com.jusfoun.hookah.core.domain.zb.mongo.MgZbProvider;
+import com.jusfoun.hookah.core.domain.zb.vo.MgZbProviderVo;
+import com.jusfoun.hookah.core.domain.zb.vo.ZbCheckVo;
 import com.jusfoun.hookah.core.generic.GenericService;
 import com.jusfoun.hookah.core.utils.ReturnData;
 
 public interface MgZbProviderService extends GenericService<MgZbProvider, String> {
 
     ReturnData userAuth(MgZbProvider provider);
+
+    ReturnData getAuthInfo(String userId);
+
+    ReturnData delAuthInfo(String userId, String optSn, String optType);
+
+    ReturnData checkAuthInfo(ZbCheckVo vo);
+
+    ReturnData getTradeRecod(String userId, String pageNumber, String pageSize);
+
+    ReturnData optAuthInfo(MgZbProviderVo vo);
 }
