d9566d0855f037ff1ad1e73671a825b21828ea35
lushanshan <409071865@qq.com>
2017-09-26 13:56:56 +0800

    默认日期七天

--- hookah-console-angular/src/apps/publish/publicController.js
+++ hookah-console-angular/src/apps/publish/publicController.js
@@ -10,8 +10,6 @@ class publicController {
               }
           });
           promise.then(function (res) {
-              console.log('数据在这里');
-              console.log(res);
               if (res.data.code == '1') {
                   var item= res.data.data.zbRequirement;
                   $scope.zbAnnexes= res.data.data.zbAnnexes;
@@ -23,12 +21,15 @@ class publicController {
                   $scope.tag=item.tag;
                   $scope.description=item.description;
                   $scope.deliveryDeadline=item.deliveryDeadline;
-                  $scope.applyDeadline=item.applyDeadline;
+                  var tomorrow = new Date();
+                  $scope.applyDeadline=new Date(tomorrow.setDate(tomorrow.getDate() + 7));
                   $scope.rewardMoney=item.rewardMoney;
                   $scope.trusteePercent=item.trusteePercent;
                   $scope.checkRemark=item.checkRemark;
                   $scope.status=item.status;
                   $scope.id=item.id;
+
+                  $scope.readerData(item.deliveryDeadline)
               } else {
 
 
@@ -40,38 +41,94 @@ class publicController {
     $scope.reader();
 
     $scope.public=function () {
-          var promise = $http({
-              method: 'GET',
-              url: $rootScope.site.crowdServer + "/api/require/updateStatus",
-              params: {
-                  id:$scope.id,
-                  status:5,
-                  applyDeadline:$filter('format')($scope.applyDeadline, 'yyyy-MM-dd HH:mm:ss')
-              }
-          });
-          promise.then(function (res, status, config, headers) {
-              console.log('数据在这里');
-              console.log(res);
-              if (res.data.code == '1') {
-                  var modalInstance =$rootScope.openConfirmDialogModal("发布成功！");
-                  modalInstance.result.then(function () {
-                      $state.go('publish.list');
-                  }, function () {
-                      $state.go('publish.list');
-                  });
-              } else {
+        if($scope.applyDeadline>$scope.deliveryDeadline){
+            console.log(555555);
+            var promise = $http({
+                method: 'GET',
+                url: $rootScope.site.crowdServer + "/api/require/updateStatus",
+                params: {
+                    id:$scope.id,
+                    status:5,
+                    applyDeadline:$filter('format')($scope.applyDeadline, 'yyyy-MM-dd HH:mm:ss')
+                }
+            });
+            promise.then(function (res, status, config, headers) {
+                console.log('数据在这里');
+                console.log(res);
+                if (res.data.code == '1') {
+                    var modalInstance =$rootScope.openConfirmDialogModal("发布成功！");
+                    modalInstance.result.then(function () {
+                        $state.go('publish.list');
+                    }, function () {
+                        $state.go('publish.list');
+                    });
+                } else {
 
-                  var modalInstance =$rootScope.openConfirmDialogModal("发布失败！");
-                  modalInstance.result.then(function () {
-                      $state.go('publish.list');
-                  }, function () {
-                      $state.go('publish.list');
-                  });
-              }
-              $rootScope.loadingState = false;
-              growl.addSuccessMessage("订单数据加载完毕。。。");
-          });
+                    var modalInstance =$rootScope.openConfirmDialogModal("发布失败！");
+                    modalInstance.result.then(function () {
+                        $state.go('publish.list');
+                    }, function () {
+                        $state.go('publish.list');
+                    });
+                }
+                $rootScope.loadingState = false;
+                growl.addSuccessMessage("订单数据加载完毕。。。");
+            });
+        }else {
+            console.log(8888);
+        }
+
       };
+
+    $scope.readerData=function (data) {
+          // 日历插件开始
+
+          $scope.inlineOptions = {
+              customClass: getDayClass,
+              minDate: new Date(),
+              maxDate: new Date(data),
+              showWeeks: true
+          };
+          $scope.open1 = function () {
+              $scope.popup1.opened = true;
+          };
+          $scope.popup1 = {
+              opened: false
+          };
+          var tomorrow = new Date();
+          tomorrow.setDate(tomorrow.getDate() + 1);
+          var afterTomorrow = new Date();
+          afterTomorrow.setDate(tomorrow.getDate() + 1);
+          $scope.events = [
+              {
+                  date: tomorrow,
+                  status: 'full'
+              },
+              {
+                  date: afterTomorrow,
+                  status: 'partially'
+              }
+          ];
+          function getDayClass(data) {
+              var date = data.date,
+                  mode = data.mode;
+              if (mode === 'day') {
+                  var dayToCheck = new Date(date).setHours(0, 0, 0, 0);
+
+                  for (var i = 0; i < $scope.events.length; i++) {
+                      var currentDay = new Date($scope.events[i].date).setHours(0, 0, 0, 0);
+
+                      if (dayToCheck === currentDay) {
+                          return $scope.events[i].status;
+                      }
+                  }
+              }
+              return '';
+          }
+
+          // 日历插件结束
+      }
+
   }
 }
 
