faf35f7ea0423045c3d30dae19867bd44f30be4d
zhaoshuai <zhaoshuai@gbdex.com>
2017-09-26 16:29:54 +0800

    评价service

---
+++ hookah-crowd-website/src/main/java/com/jusfoun/hookah/crowd/service/ZbCommentService.java
@@ -0,0 +1,10 @@
+package com.jusfoun.hookah.crowd.service;
+
+import com.jusfoun.hookah.core.domain.zb.ZbComment;
+import com.jusfoun.hookah.core.generic.GenericService;
+
+/**
+ * Created by zhaoshuai on 2017/9/26.
+ */
+public interface ZbCommentService extends GenericService<ZbComment, Long> {
+}
