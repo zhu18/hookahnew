1607530c951d13ff24af2c4fa325f6e102f310b6
zhaoshuai <zhaoshuai@gbdex.com>
2017-09-27 17:01:47 +0800

    优化我的发布需求验收

--- hookah-crowd-website/src/main/java/com/jusfoun/hookah/crowd/service/impl/ReleaseServiceImpl.java
+++ hookah-crowd-website/src/main/java/com/jusfoun/hookah/crowd/service/impl/ReleaseServiceImpl.java
@@ -85,7 +85,7 @@ public class ReleaseServiceImpl extends GenericServiceImpl<ZbRequirement, String
             }else{
                 ZbRequirement zbRequirement = zbRequireService.selectById(ment.getId());
                 if(zbRequirement != null){
-                    if(zbRequirement.getStatus() == 2){
+                    if(zbRequirement.getStatus() == ZbContants.Zb_Require_Status.CHECK_NOT.getCode().shortValue()){
                         ment.setStatus(ZbContants.Zb_Require_Status.CHECK_NOT.getCode().shortValue());
                     }
                     ment.setRequireSn(zbRequirement.getRequireSn());
@@ -342,7 +342,7 @@ public class ReleaseServiceImpl extends GenericServiceImpl<ZbRequirement, String
             if(StringUtils.isNotBlank(id.toString())){
                 filters.add(Condition.eq("id", id));
             }
-            ZbProgram zbProgram = null;
+            ZbProgram zbProgram = zbProgramService.selectOne(filters);
             zbProgram.setStatus(status);
             zbProgram.setCheckAdvice(checkAdvice);
             int i = zbProgramService.updateByCondition(zbProgram, filters);
