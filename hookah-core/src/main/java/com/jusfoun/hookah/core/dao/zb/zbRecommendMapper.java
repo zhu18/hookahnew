8b45b306db4d7404f575e638c451300f76ab9357
邓许 <mail.deng@qq.com>
2017-10-13 15:47:21 +0800

    推荐表实体类

---
+++ hookah-core/src/main/java/com/jusfoun/hookah/core/dao/zb/zbRecommendMapper.java
@@ -0,0 +1,7 @@
+package com.jusfoun.hookah.core.dao.zb;
+
+import com.jusfoun.hookah.core.domain.zb.zbRecommend;
+import com.jusfoun.hookah.core.generic.GenericDao;
+
+public interface zbRecommendMapper extends GenericDao<zbRecommend> {
+}
