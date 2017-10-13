db1ddba9051ad7b550c6d7f335fe59649295d868
邓许 <mail.deng@qq.com>
2017-10-13 14:46:26 +0800

    添加注释

--- hookah-crowd-website/src/main/java/com/jusfoun/hookah/crowd/service/MgZbRequireStatusService.java
+++ hookah-crowd-website/src/main/java/com/jusfoun/hookah/crowd/service/MgZbRequireStatusService.java
@@ -5,7 +5,18 @@ import com.jusfoun.hookah.core.generic.GenericService;
 
 public interface MgZbRequireStatusService extends GenericService<MgZbRequireStatus, String> {
 
+   /**
+    * 插入流程状态时间
+    * @param requirementSn 需求编号
+    * @param statusName    当前状态名
+    * @param statusValue   当前状态值
+    */
    void setRequireStatusInfo(String requirementSn, String statusName, String statusValue);
 
+   /**
+    * 根据需求编号查询流程状态时间
+    * @param requirementSn 需求编号
+    * @return
+    */
    MgZbRequireStatus getByRequirementSn(String requirementSn);
 }
