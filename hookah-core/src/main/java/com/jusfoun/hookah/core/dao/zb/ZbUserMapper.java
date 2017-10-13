4e171c80582cd0e62db5a1bdfb102f6214bd86f9
邓许 <mail.deng@qq.com>
2017-10-13 15:32:53 +0800

    删除无用实体类

--- hookah-core/src/main/java/com/jusfoun/hookah/core/dao/zb/ZbUserMapper.java
+++
@@ -1,8 +0,0 @@
-package com.jusfoun.hookah.core.dao.zb;
-
-import com.jusfoun.hookah.core.domain.zb.ZbUser;
-import com.jusfoun.hookah.core.generic.GenericDao;
-
-public interface ZbUserMapper extends GenericDao<ZbUser> {
-
-}
