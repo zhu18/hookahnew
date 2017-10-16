0f066477bc57d4d840096617672d40316bea8b87
zsj_x <zhaoshujun>
2017-10-16 09:13:27 +0800

    服务商-数据众包首页

--- hookah-crowd-website/src/main/resources/static/js/crowdsourcing/index.js
+++ hookah-crowd-website/src/main/resources/static/js/crowdsourcing/index.js
@@ -1,109 +1,52 @@
 /**
  * Created by Dajun on 2017-9-19.
  */
-function loadPageData(data) { //渲染页面数据
-  data = data.data.list;
-  var tempHtml = '\
-        <thead>\
-          <tr>\
-          <th>需求标题</th>\
-          <th>需求类型</th>\
-          <th>悬赏金额</th>\
-          <th>状态</th>\
-          <th>发布时间</th>\
-          <th>交付截止时间</th>\
-          <th>报名剩余时间</th>\
-          </tr>\
-          </thead>\
-          <tbody>';
-  for (var i = 0; i < data.length; i++) {
-    let tempState = '';
-    let tempType = '';
-    let tempButtonHtml = '';
-    switch (data[i].status) {
-      case 5:
-        tempState = '报名中';
-        tempButtonHtml = '<span class="signUp">我要报名</span>';
-        break;
-      case 6:
-        tempState = '报名结束';
-        tempButtonHtml = '<span class="signUp timeover">报名结束</span>';
-        break;
-    }
-    switch (data[i].type) {
-      case 1:
-        tempType = '数据采集';
-        break;
-      case 2:
-        tempType = '数据加工';
-        break;
-      case 3:
-        tempType = '数据模型';
-        break;
-      case 4:
-        tempType = '数据应用';
-        break;
-      case 5:
-        tempType = '数据清洗';
-        break;
-      case 6:
-        tempType = '其他类型';
-        break;
 
-    }
-    tempHtml += '<tr>\
-      <td><a href="javascript:void(0)">' + data[i].title + '</a></td>\
-      <td>' + tempType + '</td>\
-      <td>' + data[i].rewardMoney/100 + '</td>\
-      <td>' + tempState + '</td>\
-      <td>' + data[i].pressTime + '</td>\
-      <td>' + data[i].deliveryDeadline + '</td>\
-      <td><span class="lastTime">'+data[i].remainTime+'</span>' + tempButtonHtml + '</td>\
-      </tr>'
-  }
-  if (!data.length) {
-    tempHtml += '<tr><td style="padding: 70px;" colspan="8">----无数据----</td></tr>'
-  }
-  tempHtml += '</tbody></table>';
-  $('.crowdsourcing-table').html(tempHtml)
+/*
+ 1.数据采集--dataCollection
+ 2.数据加工--dataProcess
+ 3.数据分析--dataCleansing
+ 4.数据模型--dataModel
+ 5.数据应用--datApplication
+ 6.其他--otherData
 
-}
-
-$('.j_searchLi li').on('click', function () {
-  $(this).addClass('active').siblings().removeClass('active').parent().attr('value', $(this).attr('value'));
-  searchFn();
-});
-$('.searchBtn').on('click', function () {
-  searchFn();
-});
-function searchFn() {
-  dataParm.status = $('#status').attr('value');
-  dataParm.type = $('#type').attr('value');
-  dataParm.timeType = $('#timeType').attr('value');
-  console.log(dataParm);
-  goPage("1");
-}
-
-$(document).on('click','.signUp',function () {
-  check()
-});
-function check() {
+*/
+function renderDOM() {
   $.ajax({
-    url: '/islogin',
-    type: 'post',
+    type: 'get',
+    url: "/api/require/requirementTypeInfo",
     success: function (data) {
-      if(data){
-        $.confirm('您好！您还不是服务商，不能参加需求任务报名，如想报名请点击 【确定】 申请成为服务商，按要求提交信息即可通过服务商认证。 ',null,function(type){
-          if(type == 'yes'){
-            this.hide();
-          }else{
-            this.hide();
-
-          }
-        });
-      }else{
-        window.location.href = host.loginUrl + encodeURIComponent(host.crowd+'/crowdsourcing-list');
+      console.log(data);
+      if(data.code==1){
+        initDOM('.j_dataCollection',data.data.dataCollection);
+        initDOM('.j_dataProcess',data.data.dataProcess);
+        initDOM('.j_dataCleansing',data.data.dataCleansing);
+        initDOM('.j_dataModel',data.data.dataModel);
+        initDOM('.j_datApplication',data.data.datApplication);
+        initDOM('.j_otherData',data.data.otherData);
       }
     }
-  });
+  })
 }
+function initDOM(selector,data) {
+  let tempLiDom='';
+  for(let i=0;i<data.length;i++){
+    tempLiDom+='\
+    <li>\
+      <div class="demandDetailShow">\
+        <div class="demandDetailTitle">'+data[i].title+'</div>\
+        <img class="demandTimeIco" src="/static/images/crowdsourcing/index/demandDetail-ico.png" alt="">\
+        <div class="demandDeadData">交付截止日期：'+data[i].deliveryDeadline+'</div>\
+        <div class="demandLastTime">报名剩余时间：'+data[i].applyLastTime+'</div>\
+        <div class="demandMoney">￥'+data[i].rewardMoney+'元  </div>\
+        <div class="demandHasApply">已报名：'+data[i].count+'人</div>\
+        </div>\
+      <div class="demandDetailHide ">\
+        <div class="demandDetailCon">'+data[i].description+'</div>\
+        <a class="applyBtn demandApply" href="'+data[i].id+'">我要报名</a>\
+          </div>\
+      </li>'
+  }
+  $(selector).html(tempLiDom);
+}
+renderDOM();
