f7f88592f212547a3b722cc5f0daa383be526935
吴冰冰 <codeices@163.com>
2017-10-24 15:29:03 +0800

    完善标签管理

--- hookah-console-angular/src/apps/goods/TagsListController.js
+++ hookah-console-angular/src/apps/goods/TagsListController.js
@@ -1,22 +1,22 @@
 class TagsListController {
 	constructor($scope, $rootScope, $state, $stateParams, $http, $sce, $uibModal, usSpinnerService, growl) {
-		$scope.delTags = function(item){ //移除标签
+		$scope.delTags = function(labId){ //移除标签
 			var confirm = $rootScope.openConfirmDialogModal("确认要删除此标签吗？");
 			confirm.result.then(function () {
-				// var promise = $http({
-				// 	method: 'POST',
-				// 	url: $rootScope.site.apiServer + "/api/goods/off",
-				// 	params: {goodsId: item.goodsId}
-				// });
-				// promise.then(function (res, status, config, headers) {
-				// 	if (res.data.code == "1") {
-				// 		growl.addSuccessMessage("操作成功");
-				// 		$scope.refresh();
-				// 	}
-				// });
+				var promise = $http({
+					method: 'GET',
+					url: $rootScope.site.apiServer + "/api/label/delete",
+					params: {id: labId}
+				});
+				promise.then(function (res, status, config, headers) {
+					if (res.data.code == "1") {
+						growl.addSuccessMessage("操作成功");
+						$scope.search();
+					}
+				});
 			})
 		};//移除标签
-		$scope.addTags = function(item){//添加标签
+		$scope.addTags = function(labType){//添加标签
 			winds(null,null);
 			function winds(vals,tip){
 				var title1 = '新增标签信息';
@@ -37,28 +37,27 @@ class TagsListController {
 
 				modalInstance.result.then(function () { //模态点提交
 					if($('#checkContent').val().trim()){
-						// var testTagsOne = /^[^,](?!.*?[,]{2,}).*[^,]$/;
-						// var testTagsTwo = /^([\u4E00-\u9FA5]{1,4}|[\u4E00-\u9FA5A-Za-z0-9]{1,6}|[A-Za-z0-9]{1,8})(,([\u4E00-\u9FA5]{1,4}|[\u4E00-\u9FA5A-Za-z0-9]{1,6}|[A-Za-z0-9]{1,8}))*$/;
 						var regex ={
 							one : /^[^,](?!.*?[,]{2,}).*[^,]$/, //验证开头和结尾是否存在‘,’，并且不能有连续两个以上
 							two : /^([\u4E00-\u9FA5]{1,4}|[\u4E00-\u9FA5A-Za-z0-9]{1,6}|[A-Za-z0-9]{1,8})(,([\u4E00-\u9FA5]{1,4}|[\u4E00-\u9FA5A-Za-z0-9]{1,6}|[A-Za-z0-9]{1,8}))*$/ //验证中文为4个字，英文和数字为8个字符，中文和英文、数字组合不能超过6个
 						};
 						if(regex.one.test($('#checkContent').val())){
 							if(regex.two.test($('#checkContent').val())){
-								// var promise = $http({
-								// 	method: 'POST',
-								// 	url: $rootScope.site.apiServer + "/api/goods/forceOff",
-								// 	params: {
-								// 		goodsId: item.goodsId,
-								// 		offReason:$('#checkContent').val()
-								// 	}
-								// });
-								// promise.then(function (res, status, config, headers) {
-								// 	if (res.data.code == "1") {
-								// 		growl.addSuccessMessage("操作成功");
-								// 		$scope.refresh();
-								// 	}
-								// });
+								console.log('123123');
+								var promise = $http({
+									method: 'get',
+									url: $rootScope.site.apiServer + "/api/label/create",
+									params: {
+										type: labType,
+										labels:$('#checkContent').val()
+									}
+								});
+								promise.then(function (res, status, config, headers) {
+									if (res.data.code == "1") {
+										growl.addSuccessMessage("操作成功");
+										$scope.search();
+									}
+								});
 							}else{
 								winds($('#checkContent').val(),'two');
 							}
@@ -80,10 +79,11 @@ class TagsListController {
 			});
 			promise.then(function (res, status, config, headers) {
 				if (res.data.code == "1") {
-					
+					$scope.goodsLabel = res.data.data;
 				}
 			});
 		}
+		$scope.search()
 
 	}
 }
