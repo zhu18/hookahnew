6e67eae88745adf960198f9cf687af41b8a50a1c
邓许 <mail.deng@qq.com>
2017-10-30 17:38:14 +0800

    修改众包支付代码

---
+++ hookah-crowd-website/src/main/java/com/jusfoun/hookah/crowd/service/PayAccountService.java
@@ -0,0 +1,11 @@
+package com.jusfoun.hookah.crowd.service;
+
+import com.jusfoun.hookah.core.domain.PayAccount;
+import com.jusfoun.hookah.core.generic.GenericService;
+
+/**
+ * dx
+ */
+public interface PayAccountService extends GenericService<PayAccount, Long> {
+
+}
