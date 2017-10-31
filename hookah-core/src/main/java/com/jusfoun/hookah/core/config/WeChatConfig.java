2dec26bc00b0f7badc7c88ed3d0eba6622a51a56
lutong <lt@gbdex.com>
2017-10-31 15:03:49 +0800

    fix

--- hookah-core/src/main/java/com/jusfoun/hookah/core/config/WeChatConfig.java
+++ hookah-core/src/main/java/com/jusfoun/hookah/core/config/WeChatConfig.java
@@ -12,8 +12,10 @@ public class WeChatConfig {
     public static final String appSecret = "43ee266d02159ed8b54e6651ad3e0ca8";
 
     /*换取token链接*/
-    public static final String getAccessTokenUrl = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=APPID&secret=SECRET&code=CODE&grant_type=authorization_code";
+    public static final String getAccessTokenUrl =
+            "https://api.weixin.qq.com/sns/oauth2/access_token?appid=APPID&secret=SECRET&code=CODE&grant_type=authorization_code";
 
     /*获取用户授权链接*/
-    public static final String getSnsApiUserInfoUrl = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=APPID&redirect_uri=REDIRECT_URI&response_type=code&scope=SCOPE&state=STATE#wechat_redirect";
+    public static final String getSnsApiUserInfoUrl =
+            "https://open.weixin.qq.com/connect/oauth2/authorize?appid=APPID&redirect_uri=REDIRECT_URI&response_type=code&scope=SCOPE&state=STATE#wechat_redirect";
 }
