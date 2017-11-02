92c88549f8de1b1aefde011c34b58525359be3cb
niedefang <ndf@gbdex.com>
2017-11-02 15:37:35 +0800

    Revert "潍坊大数据交易中心项目初始化"
    This reverts commit d4cfc0c449650a3c7cfe620825d6ca464d6baaf7.

--- hookah-console-angular/src/common/common.js
+++ hookah-console-angular/src/common/common.js
@@ -3,16 +3,16 @@ var config = {
   permissionList: "",
   permissionArray:"",
   site: {
-    adminServer: "http://admin.weifangdata.com",
-    apiServer: "http://console.weifangdata.com",
-    websiteServer: "http://www.weifangdata.com",
-    authServer: "http://auth.weifangdata.com",
-    staticServer: "http://static.weifangdata.com"
+    adminServer: "http://admin.qddata.com.cn",
+    apiServer: "http://console.qddata.com.cn",
+    websiteServer: "http://trade.qddata.com.cn",
+    authServer: "http://auth.qddata.com.cn",
+    staticServer: "http://static.qddata.com.cn"
   },
   url: {
-    loginUrl: "http://auth.weifangdata.com/oauth/authorize?client_id=admin&response_type=code&redirect_uri=http://console.weifangdata.com/login&backurl=",
-    uploadUrl: "http://static.weifangdata.com/upload/fileUpload",
-    uploadEditor: "http://static.weifangdata.com/upload/wangeditor",
+    loginUrl: "http://auth.qddata.com.cn/oauth/authorize?client_id=admin&response_type=code&redirect_uri=http://console.qddata.com.cn/login&backurl=",
+    uploadUrl: "http://static.qddata.com.cn/upload/fileUpload",
+    uploadEditor: "http://static.qddata.com.cn/upload/wangeditor",
   }
 };
 
@@ -87,7 +87,7 @@ export default angular.module('Common', [
   'angular-growl',
   'angularSpinner',
 ])
-  .constant('loginUrl', "http://auth.weifangdata.com/oauth/authorize?client_id=test&response_type=code&redirect_uri=")
+  .constant('loginUrl', "http://auth.qddata.com.cn/oauth/authorize?client_id=test&response_type=code&redirect_uri=")
   .config(['growlProvider', function (growlProvider) {
     /**
      * 配置消息提示
