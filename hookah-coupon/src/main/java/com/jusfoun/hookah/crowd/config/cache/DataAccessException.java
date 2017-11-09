783a3f35b560f4c26fe4e480f9d43931377ad9e8
邓许 <mail.deng@qq.com>
2017-11-09 15:12:44 +0800

    优惠券 Module  【路通】

---
+++ hookah-coupon/src/main/java/com/jusfoun/hookah/crowd/config/cache/DataAccessException.java
@@ -0,0 +1,10 @@
+package com.jusfoun.hookah.crowd.config.cache;
+
+/**
+ * DataAccessException
+ *
+ * @author LinQ
+ * @version 2015-12-29
+ */
+public class DataAccessException extends RuntimeException {
+}
