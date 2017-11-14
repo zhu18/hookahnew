27556513ea6a6ca83d1f59d74f0896c18f94d2b9
邓许 <mail.deng@qq.com>
2017-11-14 17:34:53 +0800

    积分

---
+++ hookah-rpc-api/src/main/java/com/jusfoun/hookah/rpc/api/JfRecordService.java
@@ -0,0 +1,12 @@
+package com.jusfoun.hookah.rpc.api;
+
+import com.jusfoun.hookah.core.domain.jf.JfRecord;
+import com.jusfoun.hookah.core.generic.GenericService;
+
+/**
+ * @author dx
+ * @date 2017/4/13 下午1:46
+ * @desc
+ */
+public interface JfRecordService extends GenericService<JfRecord, Long> {
+}
