30a19f6a2444121691f159cc2378468ec26d2b5a
邓许 <mail.deng@qq.com>
2017-11-14 17:26:15 +0800

    积分抽奖模块

---
+++ hookah-integral/src/main/java/com/jusfoun/hookah/integral/config/cache/DataAccessException.java
@@ -0,0 +1,10 @@
+package com.jusfoun.hookah.coupon.config.cache;
+
+/**
+ * DataAccessException
+ *
+ * @author LinQ
+ * @version 2015-12-29
+ */
+public class DataAccessException extends RuntimeException {
+}
