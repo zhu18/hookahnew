c8d7ecc722ccbb4126d58c414463d76b71fd63ff
邓许 <mail.deng@qq.com>
2017-11-15 11:23:28 +0800

    注释

--- hookah-integral/src/main/java/com/jusfoun/hookah/integral/contants/JfEnum.java
+++ hookah-integral/src/main/java/com/jusfoun/hookah/integral/contants/JfEnum.java
@@ -1,33 +1,37 @@
-package com.jusfoun.hookah.integral.contants;
-
-import com.jusfoun.hookah.core.constants.CodeEnum;
-
-public class JfEnum {
-
-
-    private final int code;
-
-    private final String msg;
-
-    public JfEnum(int code, String msg) {
-        this.code = code;
-        this.msg = msg;
-    }
-
-    public int getCode() {
-        return code;
-    }
-
-    public String getMsg() {
-        return msg;
-    }
-
-    public static JfEnum getByCode(int code) {
-        for (JfEnum _enum : values()) {
-            if (_enum.getCode().equals(code)) {
-                return _enum;
-            }
-        }
-        return null;
-    }
-}
+//package com.jusfoun.hookah.integral.contants;
+//
+//import com.jusfoun.hookah.core.constants.CodeEnum;
+//
+//public class JfEnum {
+//
+//
+//    private final int code;
+//
+//    private final String msg;
+//
+//    public JfEnum(int code, String msg) {
+//        this.code = code;
+//        this.msg = msg;
+//    }
+//
+//    public int getCode() {
+//        return code;
+//    }
+//
+//    public String getMsg() {
+//        return msg;
+//    }
+//
+//    public static JfEnum getByCode(int code) {
+//        for (JfEnum _enum : valus()) {
+//            if (_enum.getCode().equals(code)) {
+//                return _enum;
+//            }
+//        }
+//        return null;
+//    }
+//
+//    public boolean equals(CodeEnum codeEnum){
+//        return this.getCode().equals(codeEnum.getCode());
+//    }
+//}
