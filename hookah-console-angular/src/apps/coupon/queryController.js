53d47e477925684f1e8432cba61bb269c83dc43b
lushanshan <409071865@qq.com>
2017-11-16 11:48:00 +0800

    优惠券用户

--- hookah-console-angular/src/apps/coupon/queryController.js
+++ hookah-console-angular/src/apps/coupon/queryController.js
@@ -4,15 +4,16 @@
 class queryController {
     constructor($scope, $rootScope, $http, $state, $uibModal, usSpinnerService, growl) {
         $scope.pageSizes = [               //自定定义类型数据
-            {id:-1, name:"显示条数"},
             {id:1, name:"20"},
             {id:2, name:"50"}
         ];
-        $scope.selectDates = [               //自定定义类型数据
-            {id:-1, name:"导出数据"},
-            {id:1, name:"选中用户"},
-            {id:2, name:"全部用户"}
+        $scope.exports = [               //自定定义类型数据
+            {id:"", name:"导出数据"},
+            {id:1, name:"选中用户",href:"1"},
+            {id:2, name:"全部用户",href:"2"}
         ];
+        $scope.export="";
+        $scope.pageSize=1;
         $scope.controlScreenShow=true;
         $scope.controlScreenBtn=function () { //控制筛选盒子显隐的函数
             if ($scope.controlScreenShow){
@@ -21,18 +22,14 @@ class queryController {
                 $scope.controlScreenShow=true;
 
             }
-        }
-        $scope.search = function (initCurrentPage) {
-            console.log($scope.levelStar);
+        };
+        $scope.search = function (initCurrentPage) { //Render page function
             var promise = $http({
                 method: 'GET',
-                url: $rootScope.site.apiServer + "/api/supplier/all",
+                url: $rootScope.site.apiServer + "/api/coupon/getUserCouponList",
                 params: {
-                    checkStatus: $scope.checkStatus == 0 ? '0' : ($scope.checkStatus ? $scope.checkStatus : null),//审核状态
-                    contactPhone: $scope.contactPhone ? $scope.contactPhone : null,
-                    orgName: $scope.orgName ? $scope.orgName : null,
-                    startDate: $scope.startDate ? format($scope.startDate, 'yyyy-MM-dd HH:mm:ss') : null,
-                    endDate: $scope.endDate ? format($scope.endDate, 'yyyy-MM-dd HH:mm:ss') : null,
+                    userName: $scope.userName,
+                    userType: $scope.userType,
                     currentPage: initCurrentPage == 'true' ? 1 :$rootScope.pagination.currentPage, //当前页码
                     pageSize: $rootScope.pagination.pageSize,
                 }
@@ -42,10 +39,9 @@ class queryController {
                 console.log(res);
 
                 if (res.data.code == '1') {
-                    $scope.userList = res.data.data.list;
-                    // $rootScope.pagination = res.data.data;
                     $scope.showNoneDataInfoTip = false;
                     if (res.data.data.list.length > 0) {
+                        $scope.userList = res.data.data.list;
                         if (res.data.data.totalPage > 1) {
                             $scope.showPageHelpInfo = true;
                         } else {
@@ -70,15 +66,48 @@ class queryController {
             });
 
         };
-        $scope.search('true');
+        $scope.delete=function (id) {
+            var promise = $http({
+                method: 'POST',
+                url: $rootScope.site.apiServer + "/api/coupon/delete",
+                params: {
+                    couponId:id
+                }
+            });
+            promise.then(function (res, status, config, headers) {
+                console.log('数据在这里');
+                console.log(res);
+
+                if (res.data.code == '1') {
+                    var modalInstance =$rootScope.openConfirmDialogModal("删除成功！");
+                    modalInstance.result.then(function () {
+                        $scope.search();
+                    }, function () {
+
+                    });
+                } else {
+                    var modalInstance =$rootScope.openConfirmDialogModal("删除失败！");
+                    modalInstance.result.then(function () {
+
+                    }, function () {
+
+                    });
+                }
+
+                $rootScope.loadingState = false;
+                growl.addSuccessMessage("订单数据加载完毕。。。");
+            });
+        }
+
+        $scope.search();
         $scope.refresh = function () {
             $scope.search();
         };
         $scope.pageChanged = function () {
             $scope.search();
         };
-        $scope.getDetail = function (item) {
-
+        $scope.getDetail = function (id) {//Go to detail page
+            $state.go('coupon.userDetail',{id:id})
         };
 
     }
