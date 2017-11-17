dbf2adee7bc626956fc55ddc0f5b38919b28f09a
lushanshan <409071865@qq.com>
2017-11-17 14:43:34 +0800

    添加fix+list fix

--- hookah-console-angular/src/apps/coupon/userDetailController.js
+++ hookah-console-angular/src/apps/coupon/userDetailController.js
@@ -95,6 +95,126 @@ class userDetailController {
             $scope.renderList();
         };
 
+        $scope.getDetail=function (id) {
+            var promise = $http({
+                method: 'GET',
+                url: $rootScope.site.apiServer + "/api/coupon/getUserCouponDetail",
+                params: {
+                    userCouponId:id,
+                    userId:$stateParams.id
+                }
+            });
+            promise.then(function (res, status, config, headers) {
+                if (res.data.code == '1') {
+                    var info=res.data.data;
+                    var couponType='';
+                    var applyGoods='';
+                    var applyChannel='';
+                    var receivedMode='';
+                    var applyPlatform='';
+
+                    switch (info.couponType){
+                        case 0:
+                            couponType='注册赠券';
+                            break;
+                        case 1:
+                            couponType='购物赠券';
+                            break;
+                        case 2:
+                            couponType='全场赠券';
+                            break;
+                        case 3:
+                            couponType='会员赠券';
+                            break;
+
+                    }
+
+                    switch (info.applyGoods){
+                        case 0:
+                            applyGoods='全部商品';
+                            break;
+                        case 1:
+                            applyGoods='部分商品';
+                            break;
+                        case 2:
+                            applyGoods='部分分类';
+                            break;
+                    }
+                    switch (info.applyChannel){
+                        case 0:
+                            applyChannel='无限制';
+                            break;
+                        case 1:
+                            applyChannel='满 <span>'+info.discountValue+'</span>可用';
+                            break;
+                        case 2:
+                            applyChannel='折扣';
+                            break;
+                    }
+
+                    switch (info.receivedMode){
+                        case 0:
+                            receivedMode='后台赠送';
+                            break;
+                        case 1:
+                            receivedMode='主动领取';
+                            break;
+                    }
+
+                    switch (info.applyPlatform){
+                        case 0:
+                            receivedMode='全平台';
+                            break;
+                    }
+                    var html='<table class="table">' +
+                        '<tr> ' +
+                        '<th>优惠码</th> ' +
+                        '<td> <span>'+info.couponSn+'</span> </td> ' +
+                        '<th>优惠劵名称</th> ' +
+                        '<td> <span>'+info.couponName+'</span> </td> ' +
+                        '</tr> ' +
+                        '<tr> ' +
+                        '<th>优惠券类型</th> ' +
+                        '<td> <span>'+couponType+'</span> </td> ' +
+                        '<th>可使用商品</th> ' +
+                        '<td> <span>'+applyGoods+'</span> </td> </tr> ' +
+                        '<tr> <th>使用门槛</th> ' +
+                        '<td> <span>'+applyChannel+'</span> </td> ' +
+                        '<th>面值</th> ' +
+                        '<td> <span>'+info.faceValue+'</span> </td> </tr> ' +
+                        '<tr> ' +
+                        '<th>适用平台</th> ' +
+                        '<td> <span data-ng-bind="couponName">'+applyPlatform+'</span> </td> ' +
+                        '<th>有效期</th> ' +
+                        '<td> <span data-ng-bind="couponName">'+info.expiryStartDate+'至'+info.expiryEndDate+'</span> </td> </tr> ' +
+                        '<tr> ' +
+                        '<th>领取方式</th> ' +
+                        '<td> <span data-ng-bind="couponName">'+receivedMode+'</span> </td> ' +
+                        '<th>领取时间</th> ' +
+                        '<td> <span data-ng-bind="couponName">'+info.receivedTime+'</span> </td> </tr> ' +
+                        '<tr> ' +
+                        '<th>当前状态</th> ' +
+                        '<td> <span data-ng-bind="couponName">'+info.couponName+'</span> </td> ' +
+                        '<th>使用时间</th> ' +
+                        '<td> <span data-ng-bind="couponName">'+info.usedTime+'</span> </td> ' +
+                        '</tr> ' +
+                        '<tr> ' +
+                        '<th>订单编号</th> ' +
+                        '<td colspan="3"> <span data-ng-bind="couponName">'+info.orderSn+'</span> </td> </tr> ' +
+                        '</table>';
+
+                    var modalInstance =$rootScope.openJustShowDialogModal(html);
+
+
+                } else {
+
+                }
+
+                $rootScope.loadingState = false;
+                growl.addSuccessMessage("订单数据加载完毕。。。");
+            });
+        }
+
     }
 }
 
