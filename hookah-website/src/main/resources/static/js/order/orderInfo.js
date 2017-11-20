b7834a81fc4c09c646fffa12bc8d3ebc73860a03
lushanshan <409071865@qq.com>
2017-11-20 19:03:29 +0800

    优惠券付款页面

--- hookah-website/src/main/resources/static/js/order/orderInfo.js
+++ hookah-website/src/main/resources/static/js/order/orderInfo.js
@@ -11,3 +11,29 @@ function check() {
 	// alert();
 	return true;
 }
+$(function () {
+
+    $('#coupon-list li .list-item').click(function () {
+        console.log(1);
+        var val=$("input[name='payMoney']").val();
+        if($(this).hasClass('active')){
+            $(this).removeClass('active');
+            $('#coupon-list li .list-item').addClass('active-blue');
+            $('#pay-money').html(val);
+		}else {
+            $(this).addClass('active').parent().siblings().find('.list-item').removeClass('active active-blue');
+            var id=$(this).find("input[name='couponId']").val();
+            console.log(id);
+            $("input[name='userCouponId']").val(id);
+            var faceVal=$(this).find('.j_faceValue').html();
+            var payVal=parseFloat(val)-parseFloat(faceVal);
+
+            if(payVal < 0.01){
+                payVal=0;
+			}
+            $('#pay-money').html(payVal);
+		}
+
+    });
+
+});
