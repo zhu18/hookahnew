4427944a9b6b5c5ea4eef405b000ca5c0aa9d39a
陈涛平 <ctp971412157@qq.com>
2017-11-28 14:43:01 +0800

    微信公众平台

--- hookah-core/src/main/java/com/jusfoun/hookah/core/utils/SHAUtils.java
+++ hookah-core/src/main/java/com/jusfoun/hookah/core/utils/SHAUtils.java
@@ -64,8 +64,37 @@ public class SHAUtils {
         byte[] bytes = sha.digest();  
         // 将得到的字节数组变成字符串返回  
         return byteArrayToHexString(bytes);  
-    }  
-  
+    }
+
+
+    /**
+     * SHA1 加密
+     * @param decript
+     * @return
+     */
+    public static String encryptSHA1(String decript) {
+        try {
+            MessageDigest digest = java.security.MessageDigest.getInstance(KEY_SHA1);
+            digest.update(decript.getBytes());
+            byte messageDigest[] = digest.digest();
+            // Create Hex String
+            StringBuffer hexString = new StringBuffer();
+            // 字节数组转换为 十六进制 数
+            for (int i = 0; i < messageDigest.length; i++) {
+                String shaHex = Integer.toHexString(messageDigest[i] & 0xFF);
+                if (shaHex.length() < 2) {
+                    hexString.append(0);
+                }
+                hexString.append(shaHex);
+            }
+            return hexString.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
     /** 
      * 将一个字节转化成十六进制形式的字符串 
      * @param b 字节数组 
