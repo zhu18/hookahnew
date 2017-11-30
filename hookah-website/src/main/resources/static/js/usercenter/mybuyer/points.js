592a97f72d6ca1478d3a21634e01f097cc178843
zsj_x <zhaoshujun>
2017-11-30 08:41:07 +0800

    更改事件绑定方式

--- hookah-website/src/main/resources/static/js/usercenter/mybuyer/points.js
+++ hookah-website/src/main/resources/static/js/usercenter/mybuyer/points.js
@@ -1,8 +1,6 @@
 function loadPageData(data) {
-  console.log(data);
   console.log(dataParm.type);
 if(dataParm.type==3){
-
   overTimeDetail(data)
 }else{
   var pointsListDOM=' <thead>\
@@ -49,8 +47,6 @@ if(dataParm.type==3){
   }
 }
 
-
-
 }
 
 function overTimeDetail(data) {
@@ -86,7 +82,6 @@ function overTimeDetail(data) {
 
   }
 }
-
 //点击查询按钮
 $(document).on("click", ".listNav a",function () {
   $(this).addClass('active').siblings().removeClass('active');
