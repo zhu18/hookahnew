f7c8b4cf23f35d91b1d751f310441cc6c07210de
lushanshan <409071865@qq.com>
2017-11-30 15:49:17 +0800

    结算记录

--- hookah-website/src/main/resources/static/js/usercenter/myseller/settlementRecord.js
+++ hookah-website/src/main/resources/static/js/usercenter/myseller/settlementRecord.js
@@ -1,40 +1,6 @@
 /**
  * Created by lss on 2017/7/13 0013.
  */
-
-function loadPageData(data){
-    if(data.data.list.length > 0){
-        var list = data.data.list;
-        var html = '';
-        for(var i=0; i<list.length; i++){
-            if(!list[i].goodsName){
-                list[i].goodsName=""
-            }
-            if(list[i].settleStatus== "0"){
-                list[i].settleStatus="待结算"
-            }else if(list[i].settleStatus== "1"){
-                list[i].settleStatus="已结算"
-            }
-            html+="<tr >" ;
-            html+="<td>"+list[i].orderSn+"</td>" ;
-            html+="<td>"+list[i].goodsName+"</td>" ;
-            html+="<td>￥"+(list[i].orderAmount / 100).toFixed(2)+"</td>" ;
-            html+="<td>￥"+(list[i].hasSettleAmount / 100).toFixed(2)+"</td>" ;
-            html+="<td>￥"+(list[i].noSettleAmount / 100).toFixed(2)+"</td>" ;
-            html+="<td>"+list[i].settleStatus+"</td>" ;
-            html+="<td>"+list[i].orderTime+"</td>" ;
-            html+="<td><a href='/usercenter/settlementRecordDetails?id="+list[i].id+"'>查看</a></td>";
-            html+="</tr >" ;
-        }
-        $('.settlementRecord-down-list-content').html(html);
-    }else{
-        $('.settlementRecord-down-list-content').html('<tr class="no"><td colspan="8">暂无数据</td></tr>');
-    }
-
-
-}
-
-// goPage(1);
 $(function () {
      // 状态按钮点击事件
     $(".query-condition .btn-box a").on("click",function () {
@@ -46,7 +12,7 @@ $(function () {
     })
     // 月份选择事件
     $(".query-condition .select-month a").on("click",function () {
-        $(this).addClass("active").siblings().removeClass("active")
+        $(this).addClass("active").siblings().removeClass("active");
         var html=$(this).html();
         var now = new Date();
         var date = new Date(now.getTime() - 1);
