745063604ac2461554d89391629261be7c56889d
邓许 <mail.deng@qq.com>
2017-11-30 09:59:01 +0800

    修改可用积分总额查询  积分规则修改  缓存失效

--- hookah-rpc-api/src/main/java/com/jusfoun/hookah/rpc/api/JfRuleService.java
+++ hookah-rpc-api/src/main/java/com/jusfoun/hookah/rpc/api/JfRuleService.java
@@ -2,6 +2,7 @@ package com.jusfoun.hookah.rpc.api;
 
 import com.jusfoun.hookah.core.domain.jf.JfRule;
 import com.jusfoun.hookah.core.generic.GenericService;
+import com.jusfoun.hookah.core.utils.ReturnData;
 
 /**
  * ${DESCRIPTION}
@@ -10,4 +11,21 @@ import com.jusfoun.hookah.core.generic.GenericService;
  * @create 2017-11-21 19:28
  **/
 public interface JfRuleService extends GenericService<JfRule, Integer> {
+
+    /**
+     * 修改积分规则
+     * @param id
+     * @param type
+     * @param score
+     * @param upperLimitScore
+     * @param upperLimitTime
+     * @param lowerLimitScore
+     * @param lowerLimitTime
+     * @param note
+     * @param userName
+     * @return
+     */
+    ReturnData updateRule(String id, String type, String score, String upperLimitScore,
+                          String upperLimitTime, String lowerLimitScore, String lowerLimitTime,
+                          String note, String userName) throws Exception;
 }
