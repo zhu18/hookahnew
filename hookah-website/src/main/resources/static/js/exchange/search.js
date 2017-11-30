923114f94c7bedd2fb796a61ce709c01ae7c1700
吴冰冰 <codeices@163.com>
2017-11-30 11:22:43 +0800

    fix no data image

--- hookah-website/src/main/resources/static/js/exchange/search.js
+++ hookah-website/src/main/resources/static/js/exchange/search.js
@@ -92,7 +92,7 @@ function loadPageData(data){ //渲染页面数据
 		}
 		$('.order-list ul').html(html);
 	}else {
-		$('.order-list ul').html('<div class="noData">暂无数据</div>');
+		$('.order-list ul').html('<div class="noData"><p><img src="/static/images/no_data_img.png" alt=""></p><p>商品正在上架中，敬请期待！</p></div>');
 	}
 	if(data.data2){ //渲染分类数据
 		var html = '';
