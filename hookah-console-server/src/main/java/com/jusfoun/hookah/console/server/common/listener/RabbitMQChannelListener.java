fdee888648cef9fc1ab6d25d12f55b408256e56d
邓许 <mail.deng@qq.com>
2017-12-01 17:34:14 +0800

    暂时注视

--- hookah-console-server/src/main/java/com/jusfoun/hookah/console/server/common/listener/RabbitMQChannelListener.java
+++ hookah-console-server/src/main/java/com/jusfoun/hookah/console/server/common/listener/RabbitMQChannelListener.java
@@ -1,131 +1,131 @@
-//package com.jusfoun.hookah.console.server.common.listener;
-//
-//import com.alibaba.fastjson.JSON;
-//import com.jusfoun.hookah.console.server.config.MyProps;
-//import com.jusfoun.hookah.console.server.util.PropertiesManager;
-//import com.jusfoun.hookah.core.Md5Utils;
-//import com.jusfoun.hookah.core.constants.HookahConstants;
-//import com.jusfoun.hookah.core.constants.RabbitmqQueue;
-//import com.jusfoun.hookah.core.domain.ChannelTransData;
-//import com.jusfoun.hookah.core.domain.vo.ChannelDataVo;
-//import com.jusfoun.hookah.core.domain.vo.GoodsVo;
-//import com.jusfoun.hookah.core.utils.*;
-//import com.jusfoun.hookah.rpc.api.GoodsService;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.amqp.rabbit.annotation.RabbitListener;
-//import org.springframework.stereotype.Component;
-//
-//import javax.annotation.Resource;
-//import java.util.Date;
-//import java.util.Map;
-//import java.util.Objects;
-//
-//@Component
-//public class RabbitMQChannelListener {
-//
-//    private static final Logger logger = LoggerFactory.getLogger(RabbitMQChannelListener.class);
-//
-//    @Resource
-//    GoodsService goodsService;
-//
-//    @Resource
-//    MyProps myProps;
-//
-//    @RabbitListener(queues = RabbitmqQueue.CONTRACE_CENTER_CHANNEL)
-//    public void operaPushGoods(ChannelDataVo channelDataVo) {
-//
-//        logger.info("开始执行渠道推送/撤回操作：" + (channelDataVo == null ? "未取到推送数据！": JSON.toJSONString(channelDataVo)));
-//        if (Objects.nonNull(channelDataVo)) {
-//            //推送
-//            try {
-//                channel(channelDataVo);
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//                logger.info("推送失败：" + e.getMessage());
-//            }
-//        }
-//        logger.info("结束执行渠道推送/撤回操作：" + (channelDataVo == null ? "未取到推送数据！": JSON.toJSONString(channelDataVo)));
-//
-//    }
-//
-//    private void channel(ChannelDataVo channelDataVo) throws Exception{
-//        int opera = channelDataVo.getOpera();
-//        String goodsId = channelDataVo.getGoodsId();
-//        GoodsVo goodVos = goodsService.findGoodsByIdChannel(goodsId);
-//        if(Objects.nonNull(goodVos)){
-//           Byte isPush =  goodVos.getIsPush();
-//           //判断商品是否属于推送商品
-//           if(HookahConstants.GOODS_IS_PUSH_YES.equals(isPush)){
-//               ChannelTransData channelTransData = encryptionData(goodVos,opera);
-////               Map<String, String> params = new HashedMap();
-////               params.put("transData",channelTransData.getTransData());
-////               params.put("checkCode",channelTransData.getCheckCode());
-////               params.put("timestamp",String.valueOf(channelTransData.getTimestamp()));
-//               //推送商品
-//               if(HookahConstants.CHANNEL_PUSH_OPER_PUSH == opera){
-//                   String params = JSON.toJSONString(encryptionData(goodVos,opera));
-//                   Map<String,String> resultMap = HttpClientUtil.PostMethod(PropertiesManager.getInstance().getProperty("center.system.url"),params);
-//                   if(resultMap!=null){
-////                       resMap.put("result", method.getResponseBodyAsString());
-////                       resMap.put("resultCode", String.valueOf(method.getStatusCode()));
-//                       String result = resultMap.get("result");
-//                       String resultCode = resultMap.get("resultCode");
-//                       logger.info("推送返回数据：",resultMap);
-//                       if(StringUtils.isNotBlank(resultCode) && "200".equals(resultCode)){
-//                           logger.info("推送",result);
-//                           ReturnData returnData = JSON.parseObject(result, ReturnData.class);
-//                           if(ExceptionConst.Success.equals(returnData.getCode())){
-//                               logger.info("商品[" + goodVos.getGoodsName() + "][id:" + goodsId +"]推送成功。" ,result);
-//                           } else {
-//                               logger.error("商品[" + goodVos.getGoodsName() + "][id:" + goodsId +"]推送失败。原因：" + returnData.getMessage());
-//                           }
-//                       } else {
-//                           logger.error("商品[" + goodVos.getGoodsName() + "][id:" + goodsId +"]推送失败。原因：" + result);
-//                       }
-//                   }
-//                   //撤销商品
-//               } else if(HookahConstants.CHANNEL_PUSH_OPER_CANCEL == opera){
-//                   String params = JSON.toJSONString(encryptionData(goodVos,opera));
-//                   Map<String,String> resultMap = HttpClientUtil.PostMethod(PropertiesManager.getInstance().getProperty("center.system.url"),params);
-//                   if(resultMap!=null){
-//                       String result = resultMap.get("result");
-//                       String resultCode = resultMap.get("resultCode");
-//                       logger.info("推送返回数据：",resultMap);
-//                       if(StringUtils.isNotBlank(resultCode) && "200".equals(resultCode)){
-//                           logger.info("推送",result);
-//                           ReturnData returnData = JSON.parseObject(result, ReturnData.class);
-//                           if(ExceptionConst.Success.equals(returnData.getCode())){
-//                               logger.info("商品[" + goodVos.getGoodsName() + "][id:" + goodsId +"]推送成功。" ,result);
-//                           } else {
-//                               logger.error("商品[" + goodVos.getGoodsName() + "][id:" + goodsId +"]推送失败。原因：" + returnData.getMessage());
-//                           }
-//                       } else {
-//                           logger.error("商品[" + goodVos.getGoodsName() + "][id:" + goodsId +"]推送失败。原因：" + result);
-//                       }
-//                   }
-//               }
-//           } else {
-//               logger.info("商品[" + goodVos.getGoodsName() + "][id:" + goodsId +"]不可推送。");
-//           }
-//        } else {
-//            logger.info("推送商品[id:" + goodsId +"]不存在。");
-//        }
-//    }
-//
-//    //加密数据
-//    private ChannelTransData encryptionData(Object obj, int opera){
-//        ChannelTransData data = new ChannelTransData();
-//        ChannelTransData.RelationData relationData = new ChannelTransData.RelationData();
-//        relationData.setOpera(opera);
-//        relationData.setData(obj);
-//        data.setTimestamp(new Date().getTime());
-//        String skey = PropertiesManager.getInstance().getProperty("platformCode") + HookahConstants.CHANNEL_KEY;
-//        skey += data.getTimestamp().toString().substring(16 - skey.length());
-//        data.setTransData(DESUtils.enPass(JSON.toJSONString(relationData), skey));
-//        data.setCheckCode(Md5Utils.encoderByMd5(SHAUtils.encryptSHA(JSON.toJSONString(relationData))));
-//        data.setLocalUrl(myProps.getHost().get("website"));
-//        return data;
-//    }
-//
-//}
+package com.jusfoun.hookah.console.server.common.listener;
+
+import com.alibaba.fastjson.JSON;
+import com.jusfoun.hookah.console.server.config.MyProps;
+import com.jusfoun.hookah.console.server.util.PropertiesManager;
+import com.jusfoun.hookah.core.Md5Utils;
+import com.jusfoun.hookah.core.constants.HookahConstants;
+import com.jusfoun.hookah.core.constants.RabbitmqQueue;
+import com.jusfoun.hookah.core.domain.ChannelTransData;
+import com.jusfoun.hookah.core.domain.vo.ChannelDataVo;
+import com.jusfoun.hookah.core.domain.vo.GoodsVo;
+import com.jusfoun.hookah.core.utils.*;
+import com.jusfoun.hookah.rpc.api.GoodsService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.Date;
+import java.util.Map;
+import java.util.Objects;
+
+@Component
+public class RabbitMQChannelListener {
+
+    private static final Logger logger = LoggerFactory.getLogger(RabbitMQChannelListener.class);
+
+    @Resource
+    GoodsService goodsService;
+
+    @Resource
+    MyProps myProps;
+
+    @RabbitListener(queues = RabbitmqQueue.CONTRACE_CENTER_CHANNEL)
+    public void operaPushGoods(ChannelDataVo channelDataVo) {
+
+        logger.info("开始执行渠道推送/撤回操作：" + (channelDataVo == null ? "未取到推送数据！": JSON.toJSONString(channelDataVo)));
+        if (Objects.nonNull(channelDataVo)) {
+            //推送
+            try {
+                channel(channelDataVo);
+            } catch (Exception e) {
+                e.printStackTrace();
+                logger.info("推送失败：" + e.getMessage());
+            }
+        }
+        logger.info("结束执行渠道推送/撤回操作：" + (channelDataVo == null ? "未取到推送数据！": JSON.toJSONString(channelDataVo)));
+
+    }
+
+    private void channel(ChannelDataVo channelDataVo) throws Exception{
+        int opera = channelDataVo.getOpera();
+        String goodsId = channelDataVo.getGoodsId();
+        GoodsVo goodVos = goodsService.findGoodsByIdChannel(goodsId);
+        if(Objects.nonNull(goodVos)){
+           Byte isPush =  goodVos.getIsPush();
+           //判断商品是否属于推送商品
+           if(HookahConstants.GOODS_IS_PUSH_YES.equals(isPush)){
+               ChannelTransData channelTransData = encryptionData(goodVos,opera);
+//               Map<String, String> params = new HashedMap();
+//               params.put("transData",channelTransData.getTransData());
+//               params.put("checkCode",channelTransData.getCheckCode());
+//               params.put("timestamp",String.valueOf(channelTransData.getTimestamp()));
+               //推送商品
+               if(HookahConstants.CHANNEL_PUSH_OPER_PUSH == opera){
+                   String params = JSON.toJSONString(encryptionData(goodVos,opera));
+                   Map<String,String> resultMap = HttpClientUtil.PostMethod(PropertiesManager.getInstance().getProperty("center.system.url"),params);
+                   if(resultMap!=null){
+//                       resMap.put("result", method.getResponseBodyAsString());
+//                       resMap.put("resultCode", String.valueOf(method.getStatusCode()));
+                       String result = resultMap.get("result");
+                       String resultCode = resultMap.get("resultCode");
+                       logger.info("推送返回数据：",resultMap);
+                       if(StringUtils.isNotBlank(resultCode) && "200".equals(resultCode)){
+                           logger.info("推送",result);
+                           ReturnData returnData = JSON.parseObject(result, ReturnData.class);
+                           if(ExceptionConst.Success.equals(returnData.getCode())){
+                               logger.info("商品[" + goodVos.getGoodsName() + "][id:" + goodsId +"]推送成功。" ,result);
+                           } else {
+                               logger.error("商品[" + goodVos.getGoodsName() + "][id:" + goodsId +"]推送失败。原因：" + returnData.getMessage());
+                           }
+                       } else {
+                           logger.error("商品[" + goodVos.getGoodsName() + "][id:" + goodsId +"]推送失败。原因：" + result);
+                       }
+                   }
+                   //撤销商品
+               } else if(HookahConstants.CHANNEL_PUSH_OPER_CANCEL == opera){
+                   String params = JSON.toJSONString(encryptionData(goodVos,opera));
+                   Map<String,String> resultMap = HttpClientUtil.PostMethod(PropertiesManager.getInstance().getProperty("center.system.url"),params);
+                   if(resultMap!=null){
+                       String result = resultMap.get("result");
+                       String resultCode = resultMap.get("resultCode");
+                       logger.info("推送返回数据：",resultMap);
+                       if(StringUtils.isNotBlank(resultCode) && "200".equals(resultCode)){
+                           logger.info("推送",result);
+                           ReturnData returnData = JSON.parseObject(result, ReturnData.class);
+                           if(ExceptionConst.Success.equals(returnData.getCode())){
+                               logger.info("商品[" + goodVos.getGoodsName() + "][id:" + goodsId +"]推送成功。" ,result);
+                           } else {
+                               logger.error("商品[" + goodVos.getGoodsName() + "][id:" + goodsId +"]推送失败。原因：" + returnData.getMessage());
+                           }
+                       } else {
+                           logger.error("商品[" + goodVos.getGoodsName() + "][id:" + goodsId +"]推送失败。原因：" + result);
+                       }
+                   }
+               }
+           } else {
+               logger.info("商品[" + goodVos.getGoodsName() + "][id:" + goodsId +"]不可推送。");
+           }
+        } else {
+            logger.info("推送商品[id:" + goodsId +"]不存在。");
+        }
+    }
+
+    //加密数据
+    private ChannelTransData encryptionData(Object obj, int opera){
+        ChannelTransData data = new ChannelTransData();
+        ChannelTransData.RelationData relationData = new ChannelTransData.RelationData();
+        relationData.setOpera(opera);
+        relationData.setData(obj);
+        data.setTimestamp(new Date().getTime());
+        String skey = PropertiesManager.getInstance().getProperty("platformCode") + HookahConstants.CHANNEL_KEY;
+        skey += data.getTimestamp().toString().substring(16 - skey.length());
+        data.setTransData(DESUtils.enPass(JSON.toJSONString(relationData), skey));
+        data.setCheckCode(Md5Utils.encoderByMd5(SHAUtils.encryptSHA(JSON.toJSONString(relationData))));
+        data.setLocalUrl(myProps.getHost().get("website"));
+        return data;
+    }
+
+}
--- hookah-integral/src/main/java/com/jusfoun/hookah/integral/listener/RabbitMQJFHandelListener.java
+++ hookah-integral/src/main/java/com/jusfoun/hookah/integral/listener/RabbitMQJFHandelListener.java
@@ -1,109 +1,109 @@
-package com.jusfoun.hookah.integral.listener;
-
-import com.jusfoun.hookah.core.constants.RabbitmqQueue;
-import com.jusfoun.hookah.core.domain.bo.JfBo;
-import com.jusfoun.hookah.core.domain.jf.JfRecord;
-import com.jusfoun.hookah.core.domain.jf.JfRule;
-import com.jusfoun.hookah.core.generic.Condition;
-import com.jusfoun.hookah.core.utils.StringUtils;
-import com.jusfoun.hookah.integral.contants.JfContants;
-import com.jusfoun.hookah.integral.contants.JfEnum;
-import com.jusfoun.hookah.rpc.api.CacheService;
-import com.jusfoun.hookah.rpc.api.JfRecordService;
-import com.jusfoun.hookah.rpc.api.JfRuleService;
-import com.jusfoun.hookah.rpc.api.UserService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.stereotype.Component;
-
-import javax.annotation.Resource;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
-/**
- * 处理积分消息业务
- * ps:非生产环境下由于有多个消费端，不一定是你的消费端消费消息，收不到消息正常
- */
-@Component
-public class RabbitMQJFHandelListener {
-
-    private static final Logger logger = LoggerFactory.getLogger(RabbitMQJFHandelListener.class);
-
-    @Resource
-    JfRecordService jfRecordService;
-
-    @Resource
-    UserService userService;
-
-    @Resource
-    JfRuleService jfRuleService;
-
-    @RabbitListener(queues = RabbitmqQueue.CONTRACE_JF_MSG)
-    public void jfHandle(JfBo jfBo) {
-
-        logger.info("积分消息处理BO-{}", jfBo.toString());
-
-        try {
-
-            if(jfBo == null || !StringUtils.isNotBlank(jfBo.getUserId()) || jfBo.getSourceId() == null){
-
-                logger.error("<<<<<<<积分消息处理BO为空>>>>>>>>>");
-                return;
-            }
-
-            if(userService.selectById(jfBo.getUserId()) == null){
-                logger.error("<<<<<<<用户不存在>>>>>>>");
-                return;
-            }
-
-            List<Condition> filters = new ArrayList<>();
-            filters.add(Condition.eq("sn", jfBo.getSourceId()));
-            JfRule jfRule = jfRuleService.selectOne(filters);
-            if(jfRule == null){
-                logger.error("<<<<<<<积分规则查询不存在>>>>>>>");
-                return;
-            }
-
-            JfRecord jfRecord = new JfRecord();
-            jfRecord.setUserId(jfBo.getUserId());
-            jfRecord.setSourceId(Byte.parseByte(jfBo.getSourceId() + ""));
-            jfRecord.setAction(jfRule.getAction());
-
-            if(jfRule.getUpperLimit() != null && jfRule.getScore() != null){
-
-                jfRecord.setScore(jfRule.getScore() > jfRule.getUpperLimit() ? jfRule.getUpperLimit() : jfRule.getScore());
-            }
-            jfRecord.setExpire(Byte.parseByte("0"));
-            jfRecord.setAddTime(new Date());
-            jfRecord.setOperator("System");
-            jfRecord.setAddDate(LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMM")));
-            jfRecord.setActionDesc(jfRule.getActionDesc());
-
-            if(jfRule.getSn().equals(Byte.parseByte("1"))
-                    || jfRule.getSn().equals(Byte.parseByte("2"))
-                        || jfRule.getSn().equals(Byte.parseByte("3"))){
-
-                jfRecord.setNote("有效期至" +
-                        LocalDate.now().plusYears(JfContants.JF_EXPIRE_YEAR)
-                                .format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
-            } else {
-//                jfRecord.setNote(jfBo.getNotes());
-            }
-
-            int n = jfRecordService.insertAndGetId(jfRecord);
-            if(n == 1){
-                logger.info("<<<<<<<<<<<<<<<<<<<<<<<<<<<积分消息处理成功>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-            }else {
-                logger.error("<<<<<<<<<<<<<<<<<<<<<<<<<<<积分消息处理失败>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-            }
-        }catch (Exception e){
-            logger.error("<积分消息处理失败>>>{}", e);
-        }
-
-    }
-
-}
+//package com.jusfoun.hookah.integral.listener;
+//
+//import com.jusfoun.hookah.core.constants.RabbitmqQueue;
+//import com.jusfoun.hookah.core.domain.bo.JfBo;
+//import com.jusfoun.hookah.core.domain.jf.JfRecord;
+//import com.jusfoun.hookah.core.domain.jf.JfRule;
+//import com.jusfoun.hookah.core.generic.Condition;
+//import com.jusfoun.hookah.core.utils.StringUtils;
+//import com.jusfoun.hookah.integral.contants.JfContants;
+//import com.jusfoun.hookah.integral.contants.JfEnum;
+//import com.jusfoun.hookah.rpc.api.CacheService;
+//import com.jusfoun.hookah.rpc.api.JfRecordService;
+//import com.jusfoun.hookah.rpc.api.JfRuleService;
+//import com.jusfoun.hookah.rpc.api.UserService;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.amqp.rabbit.annotation.RabbitListener;
+//import org.springframework.stereotype.Component;
+//
+//import javax.annotation.Resource;
+//import java.time.LocalDate;
+//import java.time.format.DateTimeFormatter;
+//import java.util.ArrayList;
+//import java.util.Date;
+//import java.util.List;
+//
+///**
+// * 处理积分消息业务
+// * ps:非生产环境下由于有多个消费端，不一定是你的消费端消费消息，收不到消息正常
+// */
+//@Component
+//public class RabbitMQJFHandelListener {
+//
+//    private static final Logger logger = LoggerFactory.getLogger(RabbitMQJFHandelListener.class);
+//
+//    @Resource
+//    JfRecordService jfRecordService;
+//
+//    @Resource
+//    UserService userService;
+//
+//    @Resource
+//    JfRuleService jfRuleService;
+//
+//    @RabbitListener(queues = RabbitmqQueue.CONTRACE_JF_MSG)
+//    public void jfHandle(JfBo jfBo) {
+//
+//        logger.info("积分消息处理BO-{}", jfBo.toString());
+//
+//        try {
+//
+//            if(jfBo == null || !StringUtils.isNotBlank(jfBo.getUserId()) || jfBo.getSourceId() == null){
+//
+//                logger.error("<<<<<<<积分消息处理BO为空>>>>>>>>>");
+//                return;
+//            }
+//
+//            if(userService.selectById(jfBo.getUserId()) == null){
+//                logger.error("<<<<<<<用户不存在>>>>>>>");
+//                return;
+//            }
+//
+//            List<Condition> filters = new ArrayList<>();
+//            filters.add(Condition.eq("sn", jfBo.getSourceId()));
+//            JfRule jfRule = jfRuleService.selectOne(filters);
+//            if(jfRule == null){
+//                logger.error("<<<<<<<积分规则查询不存在>>>>>>>");
+//                return;
+//            }
+//
+//            JfRecord jfRecord = new JfRecord();
+//            jfRecord.setUserId(jfBo.getUserId());
+//            jfRecord.setSourceId(Byte.parseByte(jfBo.getSourceId() + ""));
+//            jfRecord.setAction(jfRule.getAction());
+//
+//            if(jfRule.getUpperLimit() != null && jfRule.getScore() != null){
+//
+//                jfRecord.setScore(jfRule.getScore() > jfRule.getUpperLimit() ? jfRule.getUpperLimit() : jfRule.getScore());
+//            }
+//            jfRecord.setExpire(Byte.parseByte("0"));
+//            jfRecord.setAddTime(new Date());
+//            jfRecord.setOperator("System");
+//            jfRecord.setAddDate(LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMM")));
+//            jfRecord.setActionDesc(jfRule.getActionDesc());
+//
+//            if(jfRule.getSn().equals(Byte.parseByte("1"))
+//                    || jfRule.getSn().equals(Byte.parseByte("2"))
+//                        || jfRule.getSn().equals(Byte.parseByte("3"))){
+//
+//                jfRecord.setNote("有效期至" +
+//                        LocalDate.now().plusYears(JfContants.JF_EXPIRE_YEAR)
+//                                .format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+//            } else {
+////                jfRecord.setNote(jfBo.getNotes());
+//            }
+//
+//            int n = jfRecordService.insertAndGetId(jfRecord);
+//            if(n == 1){
+//                logger.info("<<<<<<<<<<<<<<<<<<<<<<<<<<<积分消息处理成功>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+//            }else {
+//                logger.error("<<<<<<<<<<<<<<<<<<<<<<<<<<<积分消息处理失败>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+//            }
+//        }catch (Exception e){
+//            logger.error("<积分消息处理失败>>>{}", e);
+//        }
+//
+//    }
+//
+//}
