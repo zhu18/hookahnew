8c790fb8ddded02af64dab05a7278c5eb470b16b
孙大伟 <fx_sdw@163.com>
2017-12-13 11:21:35 +0800

    Revert "Revert "个人中心-新增修改昵称接口""
    This reverts commit 5c7ff77c353c3b6f4c9c28e329207f4ed8e0bca0.

--- hookah-website/src/main/java/com/jusfoun/hookah/webiste/controller/UserCenterController.java
+++ hookah-website/src/main/java/com/jusfoun/hookah/webiste/controller/UserCenterController.java
@@ -414,4 +414,45 @@ public class UserCenterController {
         return  returnData;
     }
 
+    /**
+     * 个人中心-修改昵称（全局唯一，字母数字汉字下划线）
+     * @param model
+     * @return
+     */
+    @RequestMapping(value = "/updateNickName", method = RequestMethod.GET)
+    @ResponseBody
+    public ReturnData updateNickName(String nickName,Model model) {
+        ReturnData returnData = new ReturnData();
+        Session session = SecurityUtils.getSubject().getSession();
+        HashMap<String, String> userMap = (HashMap<String, String>) session.getAttribute("user");
+        if (null == userMap) {
+            returnData.setCode(ExceptionConst.Failed);
+            returnData.setMessage("请重新登录！");
+            return returnData;
+        }
+        if(StringUtils.isBlank(nickName) || ! Pattern.matches("^[\\w\\u4e00-\\u9fa5]{1,20}$",nickName)){
+            returnData.setCode(ExceptionConst.AssertFailed);
+            returnData.setMessage("昵称格式有误！");
+            return returnData;
+        }
+        List<Condition> filters = new ArrayList();
+        filters.add(Condition.eq("nickName", nickName));
+        if (userService.exists(filters)) {
+            returnData.setCode(ExceptionConst.Failed);
+            returnData.setMessage("此昵称已被占用！");
+            return  returnData;
+        }
+        User user = new User();
+        user.setNickName(nickName);
+        user.setUserId(userMap.get("userId"));
+        if (userService.updateByIdSelective(user) > 0) {
+            returnData.setCode(ExceptionConst.Success);
+            returnData.setMessage("修改成功！");
+        } else {
+            returnData.setCode(ExceptionConst.Failed);
+            returnData.setMessage("昵称修改失败！");
+        }
+        return  returnData;
+    }
+
 }
