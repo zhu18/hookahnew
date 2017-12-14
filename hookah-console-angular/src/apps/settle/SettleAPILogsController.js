560be1920e990aabce456bcf72669d5ee54386d3
joho <oopsman860818@gmail.com>
2017-12-14 14:29:12 +0800

    api查询接口调整

--- hookah-console-angular/src/apps/settle/SettleAPILogsController.js
+++ hookah-console-angular/src/apps/settle/SettleAPILogsController.js
@@ -14,7 +14,7 @@ class SettleAPILogsController {
           goodsSn: $stateParams.goodsSn,
           orderSn: $stateParams.orderSn,
           ip: $scope.ip?$scope.ip:null,
-          startDate: $scope.startDate ? format($scope.startDate, 'yyyy-MM-dd HH:mm:s') : null,
+          startDate: $scope.startDate ? format($scope.startDate, 'yyyy-MM-dd HH:mm:ss') : null,
           endDate: $scope.endDate ? format($scope.endDate, 'yyyy-MM-dd HH:mm:ss') : null,
           pageNumber:initCurrentPage == 'true' ? 1 : $rootScope.pagination.currentPage, //当前页码
           pageSize: $rootScope.pagination.pageSize
