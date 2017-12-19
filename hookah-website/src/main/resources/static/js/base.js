73040291f6234e33cbecaa0cd886988092456d24
lushanshan <409071865@qq.com>
2017-12-19 11:11:37 +0800

    Revert "Revert "bdg菜单栏向上松开鼠标不影藏问题""
    This reverts commit ab9a0aeda18f77643ea8de7710ddb7283c15b5da.

--- hookah-website/src/main/resources/static/js/base.js
+++ hookah-website/src/main/resources/static/js/base.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
 	var pathname = window.location.pathname;
 	var path = pathname.replace("/","");
-	// var host = window.location.host;
     if (path==""){
         $('#exchange_menu').show();
         $(".exchange-index-menu").show();
