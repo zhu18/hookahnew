6ba980ce7d2287ca5a563c52aee6982121f44749
dengxu <mail.deng@qq.com>
2017-12-20 14:38:10 +0800

    添加积分常量

--- hookah-core/src/main/java/com/jusfoun/hookah/core/constants/HookahConstants.java
+++ hookah-core/src/main/java/com/jusfoun/hookah/core/constants/HookahConstants.java
@@ -1,8 +1,5 @@
 package com.jusfoun.hookah.core.constants;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * Created by wangjl on 2017-3-22.
  */
@@ -728,5 +725,57 @@ public class HookahConstants {
             return code;
         }
     }
+
+    /** 新用户注册 */
+    public static final Byte JF_1 = 1;
+
+    /** 邀请新用户注册认证 */
+    public static final Byte JF_2 = 2;
+
+    /** 账号身份认证 */
+    public static final Byte JF_3 = 3;
+
+    /** 抽奖 */
+    public static final Byte JF_4 = 4;
+
+    /** 抽奖 */
+    public static final Byte JF_5 = 5;
+
+    /** VIP月度会员卡 */
+    public static final Byte JF_6 = 6;
+
+    /** 优惠券 */
+    public static final Byte JF_7 = 7;
+
+    /** 产品免费试用 */
+    public static final Byte JF_8 = 8;
+
+    /** 管理员增加 */
+    public static final Byte JF_11 = 11;
+
+    /** 管理员减少 */
+    public static final Byte JF_12 = 12;
+
+    /** 获取 */
+    public static final Byte JF_GET = 1;
+
+    /** 兑换 */
+    public static final Byte JF_TAKE = 2;
+
+    /** 管理员 */
+    public static final Byte JF_ADMIN_OPT = 3;
+
+    /** 积分过期年限 */
+    public static final Integer JF_EXPIRE_YEAR = 5;
+
+    /** 积分状态可以使用【未过期 未使用】 */
+    public static final Integer JF_EXPIRE_FALSE = 0;
+
+    /** 积分状态已过期*/
+    public static final Integer JF_EXPIRE_TRUE = 1;
+
+    /** 积分状态已使用 */
+    public static final Integer JF_EXPIRE_USED = 2;
+
 
 }
