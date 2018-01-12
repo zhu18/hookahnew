5b4513c687f9eaa68b1106e8855e08b4ff8bb898
lutong <lt@gbdex.com>
2018-01-12 14:56:12 +0800

    放开注释

--- hookah-website/src/main/java/com/jusfoun/hookah/webiste/controller/HeaderController.java
+++ hookah-website/src/main/java/com/jusfoun/hookah/webiste/controller/HeaderController.java
@@ -4,6 +4,7 @@ import com.jusfoun.hookah.core.domain.User;
 import com.jusfoun.hookah.core.domain.vo.CartVo;
 import com.jusfoun.hookah.core.generic.Condition;
 import com.jusfoun.hookah.rpc.api.CartService;
+import com.jusfoun.hookah.rpc.api.UserService;
 import org.apache.commons.beanutils.BeanUtils;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.Subject;
@@ -15,6 +16,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -32,27 +35,24 @@ public class HeaderController extends BaseController {
     CartService cartService;
 
     @RequestMapping(value = "/getHeader", method = RequestMethod.GET)
-    public String getHeader(Model model) {
+    public String getHeader(HttpServletRequest request, HttpServletResponse response) {
         try {
             Subject subject = SecurityUtils.getSubject();
             StringBuffer html = new StringBuffer("");
             if(subject.isAuthenticated()) {
                 //已登录
-                html.append("<ul class=\"text-align-right topbar-user grid-right margin-left-30\" style=\"margin-top:28px;position: relative\">");
-                html.append("<li class=\"display-inline-block margin-left-10\">");
-                html.append("<a href=\"http://auth.bdgstore.cn/logout\">退出</a></li></ul>");
-                return html.toString();
+                Map userMap = (HashMap) SecurityUtils.getSubject().getSession().getAttribute("user");
+
+                html.append("var str = '<li class=\"display-inline-block margin-left-10\"><a href=\"http://auth.bdgstore.cn/logout\">退出</a></li>';");
+                html.append("var userInfo = '<input type=\"hidden\" id=\"J_userType\" value=\""+userMap.get("userType")+"\">';");
+                html.append("document.getElementById('userStatus').innerHTML = str + userInfo;");
             }else {
-                html.append("<ul class=\"text-align-right topbar-user grid-right margin-left-30\" style=\"margin-top:28px;position: relative\">");
-                html.append("<li class=\"display-inline-block\">");
-                html.append("<a href=\"http://auth.bdgstore.cn/oauth/authorize?client_id=website&amp;response_type=code&amp;redirect_uri=\" style=\"padding:7px 14px;\">登录</a>");
-                html.append("</li>");
-                html.append("<li class=\"display-inline-block margin-left-10\">");
-                html.append("<a class=\"reg\" href=\"http://auth.bdgstore.cn/reg\">免费注册</a>");
-                html.append("</li>");
-                html.append("<p class=\"show-ad show\">注册送200元大礼<i class=\"fa fa-close\"></i></p></ul>");
-                return html.toString();
+                html.append("var str = '<li class=\"display-inline-block\"> <a href=\"http://auth.bdgstore.cn/oauth/authorize?client_id=website&amp;response_type=code&amp;redirect_uri=\" style=\"padding:7px 14px;\">登录</a> </li> <li class=\"display-inline-block margin-left-10\"> <a class=\"reg\" href=\"http://auth.bdgstore.cn/reg\">免费注册</a> </li> <p class=\"show-ad show\">注册送200元大礼 <i class=\"fa fa-close\"></i> </p>';");
+                html.append("document.getElementById('userStatus').innerHTML = str;");
+
             }
+            response.setContentType("application/x-javascript; charset=utf-8");
+            return html.toString();
         } catch (Exception e) {
             e.printStackTrace();
             logger.error(e.getMessage());
@@ -60,15 +60,13 @@ public class HeaderController extends BaseController {
         }
     }
     @RequestMapping(value = "/getCartNum", method = RequestMethod.GET)
-    public String getCart(Model model) {
+    public String getCart(HttpServletRequest request, HttpServletResponse response) {
         try {
             Subject subject = SecurityUtils.getSubject();
             StringBuffer html = new StringBuffer("");
             if(subject != null && !subject.isAuthenticated()) {
                 //未登录
-                html.append("<a href=\"/cart\">");
-                html.append("<i class=\"fa fa-shopping-cart\" style=\"color:#d20003;margin-right:5px;\"></i>购物车");
-                html.append("<span style=\"color:#000;\">0</span>件<a>");
+                html.append("document.getElementById('cartNum').innerHTML = \"0\";");
             }else if (subject != null && subject.isAuthenticated()){
                 Map userMap = (HashMap) SecurityUtils.getSubject().getSession().getAttribute("user");
                 User user = new User();
@@ -82,12 +80,9 @@ public class HeaderController extends BaseController {
                 if(cartVos != null && cartVos.size() > 0){
                     size = cartVos.stream().parallel().mapToLong(cart->cart.getGoodsNumber()).sum();
                 }
-                html.append("<a href=\"/cart\">");
-                html.append("<i class=\"fa fa-shopping-cart\" style=\"color:#d20003;margin-right:5px;\"></i>购物车");
-                html.append("<span style=\"color:#000;\">");
-                html.append(size);
-                html.append("</span>件<a>");
+                html.append("document.getElementById('cartNum').innerHTML = "+size+";");
             }
+            response.setHeader("Content-Type", "application/x-javascript; charset=utf-8");
             return html.toString();
         } catch (Exception e) {
             e.printStackTrace();
